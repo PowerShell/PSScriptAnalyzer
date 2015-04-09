e3baed7c318d191cf473cbbdb71c0ac1cdd6c099
quoctruong <quoct@microsoft.com>
2015-04-08 23:19:26 -0700

    Implement suppressing rule for function

--- Engine/Helper.cs
+++ Engine/Helper.cs
@@ -847,16 +847,6 @@ namespace Microsoft.Windows.Powershell.ScriptAnalyzer
         /// <summary>
         /// Do nothing
         /// </summary>
-        /// <param name="baseCtorInvokeMemberExpressionAst"></param>
-        /// <returns></returns>
-        public object VisitBaseCtorInvokeMemberExpression(BaseCtorInvokeMemberExpressionAst baseCtorInvokeMemberExpressionAst)
-        {
-            return null;
-        }
-
-        /// <summary>
-        /// Do nothing
-        /// </summary>
         /// <param name="configurationDefinitionAst"></param>
         /// <returns></returns>
         public object VisitConfigurationDefinition(ConfigurationDefinitionAst configurationDefinitionAst)
