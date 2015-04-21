7c051fe561fd42a4298dc9cc2da95b7566cd2368
Raghu Shantha <raghus@microsoft.com>
2015-04-21 10:49:30 -0700

    Added Unit Tests for Get-WMIObject Rule

---
+++ Tests/Rules/AvoidUsingGetWMIObject.ps1
@@ -0,0 +1,13 @@
+﻿#Script violates the rule because Get-CIMInstance is available on PS 3.0 and needs to use that
+
+#requires -version 3.0
+
+function TestFunction
+{
+    Get-WmiObject -Class Win32_ComputerSystem
+
+}
+
+TestFunction
+
+Get-WmiObject -Class Win32_OperatingSystem -Verbose
---
+++ Tests/Rules/AvoidUsingGetWMIObject.tests.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module PSScriptAnalyzer
+$getWMIObjectRuleName = "PSAvoidUsingGetWMIObject"
+$violationMessage = "File 'AvoidUsingGetWMIObject.ps1' uses Get-WMIObject. For PowerShell 3.0 and above, this is not recommended because the cmdlet is based on a non-standard DCOM protocol. Use Get-CIMInstance instead. This is CIM and WS-Man standards compliant and works in a heterogeneous environment."
+$directory = Split-Path -Parent $MyInvocation.MyCommand.Path
+$violations = Invoke-ScriptAnalyzer $directory\AvoidUsingGetWMIObject.ps1 | Where-Object {$_.RuleName -eq $getWMIObjectRuleName}
+$noViolations = Invoke-ScriptAnalyzer $directory\AvoidUsingGetWMIObjectNoViolations.ps1 | Where-Object {$_.RuleName -eq $getWMIObjectRuleName}
+
+Describe "AvoidUsingGetWMIObject" {
+    Context "Script contains references to Get-WMIObject - Violation" {
+        It "Have 2 Get-WMIObject Violations" {
+            $violations.Count | Should Be 2
+        }
+
+        It "has the correct description message for Get-WMIObject" {
+            $violations[0].Message | Should Match $violationMessage
+        }
+    }
+
+    Context "Script contains no calls to Get-WMIObject - No violation" {
+        It "results in no rule violations" {
+            $noViolations.Count | Should Be 0
+        }
+    }
+}
