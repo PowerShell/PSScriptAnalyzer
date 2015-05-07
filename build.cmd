2db020006bfb5fc1d0e214f24b94a74bb91165c1
GoodOlClint <goodolclint@gmail.com>
2015-05-07 17:59:44 -0500

    Specify build configuration and logging

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 setlocal
 if "%VS120COMNTOOLS%"=="" GOTO NOTOOLS
 call "%VS120COMNTOOLS%\VsDevCmd.bat"
-msbuild .\PSScriptAnalyzer.sln
+msbuild .\PSScriptAnalyzer.sln  /p:Configuration=Debug /l:FileLogger,Microsoft.Build.Engine;logfile=PSScriptAnalyzer_Build.log;append=true
 if NOT [%ERRORLEVEL%]==[0] pause
 
 GOTO END
