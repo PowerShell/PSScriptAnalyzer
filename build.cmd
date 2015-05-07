1a146136df98747554ae7cf967a9c6418b4f3224
GoodOlClint <goodolclint@gmail.com>
2015-05-07 17:59:02 -0500

    Check for VS Build tools

--- build.cmd
+++ build.cmd
@@ -1,6 +1,15 @@
 @echo off
 setlocal
+if "%VS120COMNTOOLS%"=="" GOTO NOTOOLS
 call "%VS120COMNTOOLS%\VsDevCmd.bat"
 msbuild .\PSScriptAnalyzer.sln
 if NOT [%ERRORLEVEL%]==[0] pause
+
+GOTO END
+
+:NOTOOLS
+echo The Visual Studio 2012 tools are not installed
+pause
+
+:END
 endlocal
