a3ec05475bde9ae1034670c753ef13fa3c8b0248
Yuting Chen <yutch@microsoft.com>
2015-05-18 16:29:00 -0700

    Update the error description string

--- Rules/Strings.Designer.cs
+++ Rules/Strings.Designer.cs
@@ -520,7 +520,7 @@ namespace Microsoft.Windows.Powershell.ScriptAnalyzer.BuiltinRules {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Some manifest fields are obsolete in the latest PowerShell version. Please update with the latest manifest fields in modules to avoid PowerShell version inconsistency..
+        ///   Looks up a localized string similar to &quot;ModuleToProcess&quot; is obsolete in the latest PowerShell version. Please update with the latest field &quot;RootModule&quot; in manifest files to avoid PowerShell version inconsistency..
         /// </summary>
         internal static string AvoidUsingDeprecatedManifestFieldsDescription {
             get {
