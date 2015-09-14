85c8b9d84bb946c50bbb0034b257088245c04b93
Quoc Truong <quoct@microsoft.com>
2015-09-14 10:59:23 -0700

    Fix test failures

--- Tests/Engine/GetScriptAnalyzerRule.tests.ps1
+++ Tests/Engine/GetScriptAnalyzerRule.tests.ps1
@@ -118,7 +118,7 @@ Describe "TestSeverity" {
 
     It "filters rules based on multiple severity inputs"{
         $rules = Get-ScriptAnalyzerRule -Severity Error,Information
-        $rules.Count | Should be 13
+        $rules.Count | Should be 14
     }
 
         It "takes lower case inputs" {
