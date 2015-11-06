26dcceba4f432f1f14232c63a7967040c3aeb6cc
Raghu Shantha [MSFT] <raghus@microsoft.com>
2015-11-06 15:26:12 -0800

    Update message in Test case for WriteHost Rule

--- Tests/Rules/AvoidUsingWriteHost.tests.ps1
+++ Tests/Rules/AvoidUsingWriteHost.tests.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module PSScriptAnalyzer
 Set-Alias ctss ConvertTo-SecureString
-$writeHostMessage = [Regex]::Escape("File 'AvoidUsingWriteHost.ps1' uses Write-Host. This is not recommended because it may not work in some hosts or there may even be no hosts at all. Use Write-Output instead.")
+$writeHostMessage = [Regex]::Escape("File 'AvoidUsingWriteHost.ps1' uses Write-Host. Avoid using Write-Host because it might not work in all hosts, does not work when there is no host, and (prior to PS 5.0) cannot be suppressed, captured, or redirected. Instead, use Write-Output, Write-Verbose, or Write-Information.")
 $writeHostName = "PSAvoidUsingWriteHost"
 $directory = Split-Path -Parent $MyInvocation.MyCommand.Path
 $violations = Invoke-ScriptAnalyzer $directory\AvoidUsingWriteHost.ps1 | Where-Object {$_.RuleName -eq $writeHostName}
