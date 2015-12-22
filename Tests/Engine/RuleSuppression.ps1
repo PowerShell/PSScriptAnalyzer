b90998ece7737008b5cda9d51217d0df5f79b901
Raghu Shantha [MSFT] <raghus@microsoft.com>
2015-12-22 15:18:51 -0800

    Fixed Tests for RuleSuppression Feature

--- Tests/Engine/RuleSuppression.ps1
+++ Tests/Engine/RuleSuppression.ps1
@@ -1,5 +1,5 @@
 ﻿[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSProvideCommentHelp", "", Scope="Function", Target="*")]
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPositionalParameters", Scope="Function", Target="Test*")]
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPositionalParameters", "", Scope="Function", Target="*")]
 Param(
 )
  
@@ -38,30 +38,5 @@ function SuppressTwoVariables()
         $b=3
         )
     {
-    }
-}
-
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "", Scope="Class")]
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("pSAvoidUsingInvokeExpression", "")]
-class TestClass 
-{
-    [void] TestFunction2()
-    {
-        Write-Host "Should not use positional parameters"
-        $a = ConvertTo-SecureString -AsPlainText "Test" -Force
-    }
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
-    [void] TestFunction()
-    {
-        Write-Host "Should not use write-host!"
-        Invoke-Expression "invoke expression"   
-    }
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingCmdletAliases","")]
-    [bool] Suppress()
-    {
-        gps
-        return $true
     }
 }
--- Tests/Engine/RuleSuppressionClass.tests.ps1
+++ Tests/Engine/RuleSuppressionClass.tests.ps1
@@ -1,4 +1,6 @@
-﻿# Check if PSScriptAnalyzer is already loaded so we don't
+﻿if ($PSVersionTable.PSVersion -ge [Version]'5.0') {
+
+# Check if PSScriptAnalyzer is already loaded so we don't
 # overwrite a test version of Invoke-ScriptAnalyzer by
 # accident
 if (!(Get-Module PSScriptAnalyzer) -and !$testingLibraryUsage)
@@ -10,14 +12,24 @@ $directory = Split-Path -Parent $MyInvocation.MyCommand.Path
 $violationsUsingScriptDefinition = Invoke-ScriptAnalyzer -ScriptDefinition (Get-Content -Raw "$directory\RuleSuppression.ps1")
 $violations = Invoke-ScriptAnalyzer "$directory\RuleSuppression.ps1"
 
-Describe "RuleSuppressionWithoutScope" {
-    Context "Function" {
+Describe "RuleSuppressionWithoutScope" {    
+
+    Context "Class" {
         It "Does not raise violations" {
-            $suppression = $violations | Where-Object { $_.RuleName -eq "PSProvideCommentHelp" }
+            $suppression = $violations | Where-Object {$_.RuleName -eq "PSAvoidUsingInvokeExpression" }
             $suppression.Count | Should Be 0
-            $suppression = $violationsUsingScriptDefinition | Where-Object { $_.RuleName -eq "PSProvideCommentHelp" }
+            $suppression = $violationsUsingScriptDefinition | Where-Object {$_.RuleName -eq "PSAvoidUsingInvokeExpression" }
             $suppression.Count | Should Be 0
         }
+    }
+
+    Context "FunctionInClass" {
+        It "Does not raise violations" {
+            $suppression = $violations | Where-Object {$_.RuleName -eq "PSAvoidUsingCmdletAliases" }
+            $suppression.Count | Should Be 0
+            $suppression = $violationsUsingScriptDefinition | Where-Object {$_.RuleName -eq "PSAvoidUsingCmdletAliases" }
+            $suppression.Count | Should Be 0
+        }        
     }
 
     Context "Script" {
@@ -48,4 +60,14 @@ Describe "RuleSuppressionWithScope" {
             $suppression.Count | Should Be 0
         }
     }
+
+    Context "ClassScope" {
+        It "Does not raise violations" {
+            $suppression = $violations | Where-Object {$_.RuleName -eq "PSAvoidUsingConvertToSecureStringWithPlainText" }
+            $suppression.Count | Should Be 0
+            $suppression = $violationsUsingScriptDefinition | Where-Object {$_.RuleName -eq "PSAvoidUsingConvertToSecureStringWithPlainText" }
+            $suppression.Count | Should Be 0
+        }
+    }
+  }
  }
