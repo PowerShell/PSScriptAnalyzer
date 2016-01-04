5dcd274fa1361c70fb3b6865491fe8d17b2dd490
Raghu Shantha [MSFT] <raghus@microsoft.com>
2016-01-04 10:29:04 -0800

    Fix for issue #417 - False positive - The variable 'OFS' is assigned but never used

--- Tests/Rules/UseDeclaredVarsMoreThanAssignmentsNoViolations.ps1
+++ Tests/Rules/UseDeclaredVarsMoreThanAssignmentsNoViolations.ps1
@@ -2,3 +2,7 @@
 Write-Ouput $declaredVars
 $script:thisshouldnotraiseerrors = "this should not raise errors"
 $foo.property = "This also should not raise errors"
+
+# Output field separator builtin special variable
+$OFS = ', '
+[string]('apple', 'banana', 'orange')
