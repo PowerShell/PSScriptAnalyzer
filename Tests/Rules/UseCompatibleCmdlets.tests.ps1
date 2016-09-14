0af41658b787fae2cd3c914642b1bd80857e4233
Kapil Borle <kborle@microsoft.com>
2016-09-13 21:41:05 -0700

    Add test files for UseCompatibleCmdlets rule

--- Tests/Rules/UseCompatibleCmdlets.tests.ps1
+++ Tests/Rules/UseCompatibleCmdlets.tests.ps1
@@ -1,9 +1,16 @@
 ﻿Import-Module PSScriptAnalyzer
 $ruleName = "UseCompatibleCmdlets"
+$directory = Split-Path $MyInvocation.MyCommand.Path -Parent
+$ruleTestDirectory = Join-Path $directory 'UseCompatibleCmdlets'
+
 
 Describe "UseCompatibleCmdlets" {
-    Context "" {
-        It "" {
+    Context "script has violation" {
+        It "detects violation" {
+            $violationFilePath = Join-Path $ruleTestDirectory 'ScriptWithViolation.ps1'
+            $settingsFilePath =  Join-Path $ruleTestDirectory 'PSScriptAnalyzerSettings.psd1'
+            $diagnosticRecords = Invoke-ScriptAnalyzer -Path $violationFilePath -IncludeRule $ruleName -Settings
+            $diagnosticRecords.Count | Should Be 1
         }
     }
 }
---
+++ Tests/Rules/UseCompatibleCmdlets/PSScriptAnalyzerSettings.psd1
@@ -0,0 +1,9 @@
+@{
+    'Rules' = @{
+        'PSUseCompatibleCmdlets' = @{
+            'mode' = 'offline',
+            'path' = '.'
+            'compatibility' = @{'core_6.0.0-alpha_windows'}
+        }
+    }
+}
