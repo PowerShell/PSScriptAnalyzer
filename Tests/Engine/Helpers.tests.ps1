6363d4c4a8cd5d2c4dde38bcfad7f5e1c92a0247
Kapil Borle <kborle@microsoft.com>
2016-09-26 13:55:41 -0700

    Add a test case for directed graph implementation

--- Tests/Engine/Helpers.tests.ps1
+++ Tests/Engine/Helpers.tests.ps1
@@ -1,6 +1,4 @@
 Import-Module PSScriptAnalyzer
-$helperNamespace = 'Microsoft.Windows.PowerShell.ScriptAnalyzer';
-
 
 Describe "Test Directed Graph" {
     Context "When a graph is created" {
@@ -9,17 +7,21 @@ Describe "Test Directed Graph" {
         $digraph.AddVertex('v2');
         $digraph.AddVertex('v3');
         $digraph.AddVertex('v4');
+        $digraph.AddVertex('v5');
+
         $digraph.AddEdge('v1', 'v2');
+        $digraph.AddEdge('v1', 'v5');
         $digraph.AddEdge('v2', 'v4');
 
         It "correctly adds the vertices" {
-            $digraph.NumVertices | Should Be 4
+            $digraph.NumVertices | Should Be 5
         }
 
         It "correctly adds the edges" {
-            $digraph.GetNumNeighbors('v1') | Should Be 1
+            $digraph.GetNumNeighbors('v1') | Should Be 2
             $neighbors = $digraph.GetNeighbors('v1')
-            $neighbors[0] | Should Be 'v2'
+            $neighbors -contains 'v2' | Should Be $true
+            $neighbors -contains 'v5' | Should Be $true
         }
 
         It "finds the connection" {
