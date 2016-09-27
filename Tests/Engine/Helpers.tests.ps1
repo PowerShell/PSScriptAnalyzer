af330077257a4b8678d1875828fa364f69801996
Kapil Borle <kborle@microsoft.com>
2016-09-27 11:23:34 -0700

    Create a digraph base class

--- Tests/Engine/Helpers.tests.ps1
+++ Tests/Engine/Helpers.tests.ps1
@@ -2,30 +2,26 @@ Import-Module PSScriptAnalyzer
 
 Describe "Test Directed Graph" {
     Context "When a graph is created" {
-        $digraph = New-Object -TypeName 'Microsoft.Windows.PowerShell.ScriptAnalyzer.DiGraph[string]'
-        $digraph.AddVertex('v1');
-        $digraph.AddVertex('v2');
-        $digraph.AddVertex('v3');
-        $digraph.AddVertex('v4');
-        $digraph.AddVertex('v5');
+        $digraph = New-Object -TypeName 'Microsoft.Windows.PowerShell.ScriptAnalyzer.DiGraph'
+        0..4 | ForEach-Object {$digraph.AddVertex()}
 
-        $digraph.AddEdge('v1', 'v2');
-        $digraph.AddEdge('v1', 'v5');
-        $digraph.AddEdge('v2', 'v4');
+        $digraph.AddEdge(0, 1);
+        $digraph.AddEdge(0, 4);
+        $digraph.AddEdge(1, 3);
 
         It "correctly adds the vertices" {
-            $digraph.NumVertices | Should Be 5
+            $digraph.GetNumVertices() | Should Be 5
         }
 
         It "correctly adds the edges" {
-            $digraph.GetNumNeighbors('v1') | Should Be 2
-            $neighbors = $digraph.GetNeighbors('v1')
-            $neighbors -contains 'v2' | Should Be $true
-            $neighbors -contains 'v5' | Should Be $true
+            $digraph.GetNumNeighbors(0) | Should Be 2
+            $neighbors = $digraph.GetNeighbors(0)
+            $neighbors -contains 1 | Should Be $true
+            $neighbors -contains 4 | Should Be $true
         }
 
         It "finds the connection" {
-            $digraph.IsConnected('v1', 'v4') | Should Be $true
+            $digraph.IsConnected(0, 3) | Should Be $true
         }
     }
 }
