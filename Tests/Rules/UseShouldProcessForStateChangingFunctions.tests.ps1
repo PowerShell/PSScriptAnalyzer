a9924e156f916f5f15736839bd9f7e59d9f47cfd
Kapil Borle <kborle@microsoft.com>
2016-10-11 13:34:38 -0700

    Add tests for each state changing verb

--- Tests/Rules/UseShouldProcessForStateChangingFunctions.tests.ps1
+++ Tests/Rules/UseShouldProcessForStateChangingFunctions.tests.ps1
@@ -6,6 +6,23 @@ $violations = Invoke-ScriptAnalyzer $directory\UseShouldProcessForStateChangingF
 $noViolations = Invoke-ScriptAnalyzer $directory\UseShouldProcessForStateChangingFunctionsNoViolations.ps1 | Where-Object {$_.RuleName -eq $violationName}
 
 Describe "It checks UseShouldProcess is enabled when there are state changing verbs in the function names" {
+    Context "When function name has state changing verb" {
+
+        Function Test-Verb([string] $verb)
+        {
+            $scriptDefinitionGeneric = @'
+Function New-{0} () {{ }}
+'@
+            $scriptDef = $scriptDefinitionGeneric -f $verb
+            It ('finds "{0}" verb in function name' -f $verb) {
+                $violations = Invoke-ScriptAnalyzer -ScriptDefinition $scriptDef -IncludeRule $violationName
+                $violations.Count | Should Be 1
+            }
+        }
+
+        $verbs = @('New', 'Set', 'Remove', 'Start', 'Stop', 'Restart', 'Reset', 'Update')
+        $verbs | ForEach-Object {Test-Verb $_}
+    }
     Context "When there are violations" {
     	$numViolations = 5
         It ("has {0} violations where ShouldProcess is not supported" -f $numViolations) {
@@ -16,9 +33,9 @@ Describe "It checks UseShouldProcess is enabled when there are state changing ve
             $violations[0].Message | Should Match $violationMessage
         }
 
-	It "has the correct extent" {
-	   $violations[0].Extent.Text | Should Be "Set-MyObject"
-	}
+        It "has the correct extent" {
+        $violations[0].Extent.Text | Should Be "Set-MyObject"
+        }
     }
 
     Context "When there are no violations" {
