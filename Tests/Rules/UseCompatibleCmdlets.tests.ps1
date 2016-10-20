5cbc82fcda2a1080428d931d973a626b8ca15966
Kapil Borle <kborle@microsoft.com>
2016-10-19 17:12:39 -0700

    Update UseCompatibleCmdlets rule tests

--- Tests/Rules/UseCompatibleCmdlets.tests.ps1
+++ Tests/Rules/UseCompatibleCmdlets.tests.ps1
@@ -1,7 +1,10 @@
-﻿Import-Module PSScriptAnalyzer
-$ruleName = "PSUseCompatibleCmdlets"
+﻿$ruleName = "PSUseCompatibleCmdlets"
 $directory = Split-Path $MyInvocation.MyCommand.Path -Parent
+$testRootDirectory = Split-Path -Parent $directory
 $ruleTestDirectory = Join-Path $directory 'UseCompatibleCmdlets'
+
+Import-Module PSScriptAnalyzer
+Import-Module (Join-Path $testRootDirectory 'PSScriptAnalyzerTestHelper.psm1')
 
 Describe "UseCompatibleCmdlets" {
     Context "script has violation" {
@@ -11,5 +14,45 @@ Describe "UseCompatibleCmdlets" {
             $diagnosticRecords = Invoke-ScriptAnalyzer -Path $violationFilePath -IncludeRule $ruleName -Settings $settingsFilePath
             $diagnosticRecords.Count | Should Be 1
         }
+    }
+
+    Function Test-Command
+    {
+        param (
+            [Parameter(ValueFromPipeline)]
+            $command,
+            $settings,
+            $expectedViolations
+        )
+        process
+        {
+            It ("found {0} violations for '{1}'" -f $expectedViolations, $command) {
+                Invoke-ScriptAnalyzer -ScriptDefinition $command -IncludeRule $ruleName -Settings $settings | `
+                    Get-Count | `
+                    Should Be $expectedViolations
+            }
+        }
+    }
+
+    $settings = @{rules=@{PSUseCompatibleCmdlets=@{compatibility=@("core-6.0.0-alpha-windows")}}}
+
+    Context "Microsoft.PowerShell.Core" {
+         @('Enter-PSSession', 'Foreach-Object', 'Get-Command') | `
+            Test-Command -Settings $settings -ExpectedViolations 0
+    }
+
+    Context "Non-builtin commands" {
+        @('get-foo', 'get-bar', 'get-baz') | `
+            Test-Command -Settings $settings -ExpectedViolations 0
+    }
+
+    Context "Aliases" {
+        @('where', 'select', 'cd') | `
+            Test-Command -Settings $settings -ExpectedViolations 0
+    }
+
+    Context "Commands present in reference platform but not in target platform" {
+        @("Start-VM", "New-SmbShare", "Get-Disk") | `
+            Test-Command -Settings $settings -ExpectedViolations 1
     }
 }
