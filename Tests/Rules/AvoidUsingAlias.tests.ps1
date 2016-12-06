9d3daab19594d1031e905a9a9fff2ca369412b63
Kapil Borle <kborle@microsoft.com>
2016-12-06 15:47:02 -0800

    Fix failing test

--- Tests/Rules/AvoidUsingAlias.tests.ps1
+++ Tests/Rules/AvoidUsingAlias.tests.ps1
@@ -1,11 +1,13 @@
-﻿Import-Module PSScriptAnalyzer
-$violationMessage = "'cls' is an alias of 'Clear-Host'. Alias can introduce possible problems and make scripts hard to maintain. Please consider changing alias to its full content."
+﻿$violationMessage = "'cls' is an alias of 'Clear-Host'. Alias can introduce possible problems and make scripts hard to maintain. Please consider changing alias to its full content."
 $violationName = "PSAvoidUsingCmdletAliases"
 $directory = Split-Path -Parent $MyInvocation.MyCommand.Path
+$testRootDirectory = Split-Path -Parent $directory
 $violationFilepath = Join-Path $directory 'AvoidUsingAlias.ps1'
 $violations = Invoke-ScriptAnalyzer $violationFilepath | Where-Object {$_.RuleName -eq $violationName}
 $noViolations = Invoke-ScriptAnalyzer $directory\AvoidUsingAliasNoViolations.ps1 | Where-Object {$_.RuleName -eq $violationName}
-Import-Module (Join-Path $directory "PSScriptAnalyzerTestHelper.psm1")
+
+Import-Module PSScriptAnalyzer
+Import-Module (Join-Path $testRootDirectory "PSScriptAnalyzerTestHelper.psm1")
 
 Describe "AvoidUsingAlias" {
     Context "When there are violations" {
