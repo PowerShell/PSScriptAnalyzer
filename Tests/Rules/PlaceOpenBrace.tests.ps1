ae8d06a8b4b9d968b23a040deacca4ebe19b3fc1
Kapil Borle <kborle@microsoft.com>
2016-12-23 17:44:58 -0800

    Add test for PlaceOpenBrace rule

--- Tests/Rules/PlaceOpenBrace.tests.ps1
+++ Tests/Rules/PlaceOpenBrace.tests.ps1
@@ -1,9 +1,24 @@
 ﻿Import-Module PSScriptAnalyzer
-$ruleName = "PlaceOpenBrace"
+$ruleName = "PSPlaceOpenBrace"
 
-Describe "PlaceOpenBrace" {
-    Context "" {
-        It "" {
+Describe "PlaceOpenBrace on same line" {
+    Context "When a open brace is on a new line" {
+        BeforeAll{
+            $def = @'
+function foo ($param1)
+{
+
+}
+'@
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -IncludeRule $ruleName
+        }
+
+        It "Should find a violation" {
+            $violations.Count | Should Be 1
+        }
+
+        It "Should mark only the open brace" {
+            $violations[0].Extent.Text | Should Be '{'
         }
     }
 }
