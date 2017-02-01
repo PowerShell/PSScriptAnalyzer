5cbe9a1bdb2c7324b0554a34b3b75ccc3e185563
Kapil Borle <kborle@microsoft.com>
2017-01-31 21:04:09 -0800

    Add tests for operator violations

--- Tests/Rules/UseWhitespace.tests.ps1
+++ Tests/Rules/UseWhitespace.tests.ps1
@@ -2,8 +2,9 @@
 $ruleName = "PSUseWhitespace"
 $ruleConfiguration = @{
     Enable = $true
-    CheckOpenBrace = $true
-    CheckOpenParen = $true
+    CheckOpenBrace = $false
+    CheckOpenParen = $false
+    CheckOperator = $false
 }
 
 $settings = @{
@@ -14,83 +15,123 @@ $settings = @{
 }
 
 Describe "UseWhitespace" {
-    Context "When no whitespace is present before if block open brace" {
+    Context "When an open brace follows a keyword" {
         BeforeAll {
+
+            $ruleConfiguration.CheckOpenBrace = $true
+            $ruleConfiguration.CheckOpenParen = $false
+            $ruleConfiguration.CheckOperator = $false
+        }
+
+        It "Should find a violation if an open brace does not follow whitespace" {
             $def = @'
 if ($true){}
 '@
-            $ruleConfiguration.CheckOpenParen = $false
-            $ruleConfiguration.CheckOpenBrace = $true
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
-        }
-
-        It "Should find a violation" {
             $violations.Count | Should Be 1
         }
-    }
 
-    Context "When no whitespace is present before open paren of an if block" {
-        BeforeAll {
+        It "Should find no violation if an open brace follows a whitespace" {
             $def = @'
 if($true) {}
 '@
-            $ruleConfiguration.CheckOpenParen = $true
-            $ruleConfiguration.CheckOpenBrace = $false
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations.Count | Should Be 0
+
         }
 
-        It "Should find a violation" {
-            $violations.Count | Should Be 1
-        }
     }
 
-    Context "When no whitespace is present before open paren of a function definition" {
+    Context "When a parenthesis follows a keyword" {
         BeforeAll {
+            $ruleConfiguration.CheckOpenBrace = $false
+            $ruleConfiguration.CheckOpenParen = $true
+            $ruleConfiguration.CheckOperator = $false
+        }
+
+        It "Should find no violation if an open brace follows a whitespace" {
+            $def = @'
+if($true) {}
+'@
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations.Count | Should Be 1
+        }
+
+        It "Should not find a violation if no whitespace is present before open paren of a function definition" {
             $def = @'
 function foo($param1) {
 
 }
 '@
-            $ruleConfiguration.CheckOpenParen = $true
-            $ruleConfiguration.CheckOpenBrace = $false
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
-        }
-
-                It "Should not find a violation" {
             $violations.Count | Should Be 0
         }
-    }
 
-    Context "When no whitespace is present before open of a param block" {
-        BeforeAll {
+        It "Should not find a violation if no whitespace is present before open paren of a param block" {
             $def = @'
 function foo() {
     param( )
 }
 '@
-            $ruleConfiguration.CheckOpenParen = $true
-            $ruleConfiguration.CheckOpenBrace = $false
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
-        }
-
-                It "Should not find a violation" {
             $violations.Count | Should Be 0
         }
-    }
 
-        Context "When no whitespace is present in a nested open paren" {
-        BeforeAll {
+        It "Should not find a violation if no whitespace is present in a nested open paren" {
             $def = @'
 function foo($param) {
     ((Get-Process))
 }
 '@
-            $ruleConfiguration.CheckOpenParen = $true
-            $ruleConfiguration.CheckOpenBrace = $false
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations.Count | Should Be 0
+        }
+    }
+
+    Context "When there is whitespace around assignment and binary operators" {
+        BeforeAll {
+            $ruleConfiguration.CheckOpenParen = $false
+            $ruleConfiguration.CheckOpenBrace = $false
+            $ruleConfiguration.CheckOperator = $true
         }
 
-                It "Should not find a violation" {
+        It "Should find a violation if no whitespace around an assignment operator" {
+            $def = @'
+$x=1
+'@
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations.Count | Should Be 1
+        }
+
+        It "Should find a violation if no whitespace before an assignment operator" {
+            $def = @'
+$x= 1
+'@
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations.Count | Should Be 1
+        }
+
+        It "Should find a violation if no whitespace after an assignment operator" {
+            $def = @'
+$x =1
+'@
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations.Count | Should Be 1
+        }
+
+        It "Should find a violation if there is a whitespaces not of size 1 around an assignment operator" {
+            $def = @'
+$x  =  1
+'@
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations.Count | Should Be 1
+        }
+
+        It "Should find no violation if there are whitespaces of size 1 around an assignment operator" {
+            $def = @'
+$x = 1
+'@
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
             $violations.Count | Should Be 0
         }
     }
