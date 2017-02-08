7755e787f98f78832e6e0a981cd358cb83087661
Kapil Borle <kborle@microsoft.com>
2017-02-07 21:11:15 -0800

    Add methods to get one line if-else braces

--- Engine/TokenOperations.cs
+++ Engine/TokenOperations.cs
@@ -110,6 +110,86 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer
             }
         }
 
+        public IEnumerable<Token> GetCloseBraceInOneLineIfStatement()
+        {
+            return GetBraceInOneLineIfStatment(TokenKind.RCurly);
+        }
+
+        public IEnumerable<Token> GetOpenBraceInOneLineIfStatement()
+        {
+            return GetBraceInOneLineIfStatment(TokenKind.LCurly);
+        }
+
+        // TODO Fix code duplication in the following method and GetBraceInCommandElement
+        private IEnumerable<Token> GetBraceInOneLineIfStatment(TokenKind tokenKind)
+        {
+            var ifStatementAsts = ast.FindAll(ast =>
+            {
+                var ifAst = ast as IfStatementAst;
+                if (ifAst == null)
+                {
+                    return false;
+                }
+
+                return ifAst.Extent.StartLineNumber == ifAst.Extent.EndLineNumber;
+            },
+            true);
+
+            if (ifStatementAsts == null)
+            {
+                yield break;
+            }
+
+            var braceTokens = new List<Token>();
+            foreach (var ast in ifStatementAsts)
+            {
+                var ifStatementAst = ast as IfStatementAst;
+                foreach (var clause in ifStatementAst.Clauses)
+                {
+                    var tokenIf
+                        = tokenKind == TokenKind.LCurly
+                            ? GetTokens(clause.Item2).FirstOrDefault()
+                            : GetTokens(clause.Item2).LastOrDefault();
+                    if (tokenIf != null)
+                    {
+                        yield return tokenIf;
+                    }
+                }
+
+                if (ifStatementAst.ElseClause == null)
+                {
+                    continue;
+                }
+
+                var tokenElse
+                    = tokenKind == TokenKind.LCurly
+                            ? GetTokens(ifStatementAst.ElseClause).FirstOrDefault()
+                            : GetTokens(ifStatementAst.ElseClause).LastOrDefault();
+                if (tokenElse != null)
+                {
+                    yield return tokenElse;
+                }
+            }
+        }
+
+        private IEnumerable<Token> GetTokens(Ast ast)
+        {
+            int k = 0;
+            while (k < tokens.Length && tokens[k].Extent.EndOffset <= ast.Extent.StartOffset)
+            {
+                k++;
+            }
+
+            while (k < tokens.Length && tokens[k].Extent.EndOffset <= ast.Extent.EndOffset)
+            {
+                var token = tokens[k++];
+                if (token.Extent.StartOffset >= ast.Extent.StartOffset)
+                {
+                    yield return token;
+                }
+            }
+        }
+
         public IEnumerable<LinkedListNode<Token>> GetTokenNodes(TokenKind kind)
         {
             return GetTokenNodes((token) => token.Kind == kind);
