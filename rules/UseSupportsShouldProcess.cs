13641725f51d1b2d2c0bbebc6f23114796197d00
Kapil Borle <kborle@microsoft.com>
2017-03-31 16:59:43 -0700

    Add basic checks for whatif and confirm

--- rules/UseSupportsShouldProcess.cs
+++ rules/UseSupportsShouldProcess.cs
@@ -14,41 +14,94 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 #endif
 using System.Globalization;
-using System.Linq;
 using System.Management.Automation.Language;
 using Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic;
 
 namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
 {
     /// <summary>
-    /// A class to walk an AST to check for [violation]
+    /// A class to that implements the UseSupportsShouldProcess rule.
     /// </summary>
     #if !CORECLR
     [Export(typeof(IScriptRule))]
 #endif
-    class UseSupportsShouldProcess : IScriptRule
+    class UseSupportsShouldProcess : ConfigurableRule
     {
         /// <summary>
-        /// Analyzes the given ast to find the [violation]
+        /// Analyzes the given ast to find if a function defines Confirm and/or WhatIf parameters manually
+        /// instead of using SupportShouldProcess attribute.
         /// </summary>
         /// <param name="ast">AST to be analyzed. This should be non-null</param>
         /// <param name="fileName">Name of file that corresponds to the input AST.</param>
         /// <returns>A an enumerable type containing the violations</returns>
-        public IEnumerable<DiagnosticRecord> AnalyzeScript(Ast ast, string fileName)
+        public override IEnumerable<DiagnosticRecord> AnalyzeScript(Ast ast, string fileName)
         {
             if (ast == null)
             {
-                throw new ArgumentNullException("ast");
+                throw new ArgumentNullException(nameof(ast));
             }
 
             // your code goes here
-            yield return new DiagnosticRecord();
+            return FindViolations(ast);
+        }
+
+        private List<DiagnosticRecord> FindViolations(Ast ast)
+        {
+            var foundAsts = ast.FindAll(x => x is FunctionDefinitionAst, true);
+            var diagnosticRecords = new List<DiagnosticRecord>();
+            foreach (var foundAst in foundAsts)
+            {
+                var functionDefinitionAst = foundAst as FunctionDefinitionAst;
+                if (AddsWhatIf(functionDefinitionAst) || AddsConfirm(functionDefinitionAst))
+                {
+                    diagnosticRecords.Add(new DiagnosticRecord());
+                }
+            }
+
+            return diagnosticRecords;
+        }
+
+        private bool AddsConfirm(FunctionDefinitionAst functionDefinitionAst)
+        {
+            return AddsParameter(functionDefinitionAst, "confirm");
+        }
+
+        private bool AddsWhatIf(FunctionDefinitionAst functionDefinitionAst)
+        {
+            return AddsParameter(functionDefinitionAst, "whatif");
+        }
+
+        private bool AddsParameter(FunctionDefinitionAst functionDefinitionAst, string parameter)
+        {
+            if (functionDefinitionAst.Parameters != null)
+            {
+                return ParametersContain(functionDefinitionAst.Parameters, parameter);
+            }
+            else if (functionDefinitionAst.Body.ParamBlock?.Parameters != null)
+            {
+                return ParametersContain(functionDefinitionAst.Body.ParamBlock.Parameters, parameter);
+            }
+
+            return false;
+        }
+
+        private bool ParametersContain(ICollection<ParameterAst> parameterAsts, string parameter)
+        {
+            foreach (var paramAst in parameterAsts)
+            {
+                if(paramAst.Name.VariablePath.UserPath.Equals(parameter, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         /// <summary>
         /// Retrieves the common name of this rule.
         /// </summary>
-        public string GetCommonName()
+        public override string GetCommonName()
         {
             return string.Format(CultureInfo.CurrentCulture, Strings.UseSupportsShouldProcessCommonName);
         }
@@ -56,7 +109,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <summary>
         /// Retrieves the description of this rule.
         /// </summary>
-        public string GetDescription()
+        public override string GetDescription()
         {
             return string.Format(CultureInfo.CurrentCulture, Strings.UseSupportsShouldProcessDescription);
         }
@@ -64,7 +117,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <summary>
         /// Retrieves the name of this rule.
         /// </summary>
-        public string GetName()
+        public override string GetName()
         {
             return string.Format(
                 CultureInfo.CurrentCulture,
@@ -74,9 +127,25 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         }
 
         /// <summary>
+        /// Retrieves the name of the module/assembly the rule is from.
+        /// </summary>
+        public override string GetSourceName()
+        {
+            return string.Format(CultureInfo.CurrentCulture, Strings.SourceName);
+        }
+
+        /// <summary>
+        /// Retrieves the type of the rule, Builtin, Managed or Module.
+        /// </summary>
+        public override SourceType GetSourceType()
+        {
+            return SourceType.Builtin;
+        }
+
+        /// <summary>
         /// Retrieves the severity of the rule: error, warning or information.
         /// </summary>
-        public RuleSeverity GetSeverity()
+        public override RuleSeverity GetSeverity()
         {
             return RuleSeverity.Warning;
         }
@@ -88,22 +157,6 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         public DiagnosticSeverity GetDiagnosticSeverity()
         {
             return DiagnosticSeverity.Warning;
-        }
-
-        /// <summary>
-        /// Retrieves the name of the module/assembly the rule is from.
-        /// </summary>
-        public string GetSourceName()
-        {
-            return string.Format(CultureInfo.CurrentCulture, Strings.SourceName);
-        }
-
-        /// <summary>
-        /// Retrieves the type of the rule, Builtin, Managed or Module.
-        /// </summary>
-        public SourceType GetSourceType()
-        {
-            return SourceType.Builtin;
         }
     }
 }
