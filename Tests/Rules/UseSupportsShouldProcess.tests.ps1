634056f4d77b2db0d20edbadc34b21e410692fb6
Kapil Borle <kborle@microsoft.com>
2017-03-31 17:00:23 -0700

    Add tests for manual adition of whatif and confirm

--- Tests/Rules/UseSupportsShouldProcess.tests.ps1
+++ Tests/Rules/UseSupportsShouldProcess.tests.ps1
@@ -1,9 +1,59 @@
 ﻿Import-Module PSScriptAnalyzer
-$ruleName = "UseSupportsShouldProcess"
+$ruleName = "PSUseSupportsShouldProcess"
+$settings = @{
+    IncludeRules = @("PSUseSupportsShouldProcess")
+    Rules = @{
+        PSUseSupportsShouldProcess = @{
+            Enable = $true
+        }
+    }
+}
 
 Describe "UseSupportsShouldProcess" {
-    Context "" {
-        It "" {
+    Context "When a function manually adds WhatIf and Confirm parameters" {
+        It "Should find violation if only WhatIf is added" {
+            $def = @'
+function foo {
+    param(
+        [bool] $whatif
+    )
+}
+'@
+            $violation = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violation.Count | Should Be 1
+        }
+
+        It "Should find violation if only Confirm is added" {
+            $def = @'
+function foo {
+    param(
+        [bool] $confirm
+    )
+}
+'@
+            $violation = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violation.Count | Should Be 1
+        }
+
+        It "Should find violation if both Whatif and Confirm are added" {
+            $def = @'
+function foo {
+    param(
+        [bool] $confirm,
+        [bool] $whatif
+    )
+}
+'@
+            $violation = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violation.Count | Should Be 1
+        }
+
+        It "Suggests adding SupportsShouldProcess attribute" {
+
+        }
+
+        It "Suggests removing the manually added parameters" {
+
         }
     }
 }
