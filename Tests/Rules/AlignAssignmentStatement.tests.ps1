3f2e1046d15479d42b091b79eff52772ad75d458
Kapil Borle <kborle@microsoft.com>
2017-04-12 10:15:00 -0700

    Add initial tests for AlignAssignmentStatement rule

--- Tests/Rules/AlignAssignmentStatement.tests.ps1
+++ Tests/Rules/AlignAssignmentStatement.tests.ps1
@@ -1,9 +1,41 @@
-﻿Import-Module PSScriptAnalyzer
-$ruleName = "AlignAssignmentStatement"
+﻿$directory = Split-Path -Parent $MyInvocation.MyCommand.Path
+$testRootDirectory = Split-Path -Parent $directory
+
+Import-Module PSScriptAnalyzer
+Import-Module (Join-Path $testRootDirectory "PSScriptAnalyzerTestHelper.psm1")
+
+$ruleConfiguration = @{
+    Enable = $true
+    AlignInHashtable = $true
+    AlignInDSCConfiguration = $true
+}
+
+$settings = @{
+    IncludeRules = @("PSAlignAssignmentStatement")
+    Rules = @{
+        PSAlignAssignmentStatement = $ruleConfiguration
+    }
+}
 
 Describe "AlignAssignmentStatement" {
-    Context "" {
-        It "" {
+    Context "Hashtable" {
+        It "Should align assignment statements in a hashtable" {
+            $def = @'
+$hashtable = @{
+    property1 = "value"
+    anotherProperty = "another value"
+}
+'@
+
+# Expected output should be the following
+# $hashtable = @{
+#     property1       = "value"
+#     anotherProperty = "another value"
+# }
+
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations.Count | Should Be 1
+            Test-CorrectionExtentFromContent $def $violations 1 '' '      '
         }
     }
 }
