5db911034a30733c668bfeacaca2543b532e2d98
Kapil Borle <kborle@microsoft.com>
2017-04-17 17:09:48 -0700

    Add more IScriptExtension.Contains tests

--- Tests/Engine/Extensions.texts.ps1
+++ Tests/Engine/Extensions.texts.ps1
@@ -1,18 +1,40 @@
 Import-Module PSScriptAnalyzer
 
 Describe "IScriptExtent Extension Methods" {
-    Context "When single line inner extent is strictly contained in a single line outer extent" {
-        It "Should return true" {
-           $outer = "This is the outer string"
-           $outerExtentStartPos = New-Object -TypeName 'System.Management.Automation.Language.ScriptPosition' -ArgumentList $null,1,1,$outer,$outer
-           $outerExtentEndPos = New-Object -TypeName 'System.Management.Automation.Language.ScriptPosition' -ArgumentList $null,1,$outer.Length,$outer,$outer
-           $outerExtent = New-Object -TypeName 'System.Management.Automation.Language.ScriptExtent' -ArgumentList $outerExtentStartPos,$outerExtentEndPos
+    Context "When single line inner extent is compared with a single line outer extent" {
+        BeforeAll {
+            $outer = "This is the outer string"
+            $scriptPositionType = 'System.Management.Automation.Language.ScriptPosition'
+            $scriptExtentType = 'System.Management.Automation.Language.ScriptExtent'
 
-           $innerExtentStartPos = New-Object -TypeName 'System.Management.Automation.Language.ScriptPosition' -ArgumentList $null,1,1,$outer,$outer
-           $innerExtentEndPos = New-Object -TypeName 'System.Management.Automation.Language.ScriptPosition' -ArgumentList $null,1,2,$outer,$outer
-           $innerExtent = New-Object -TypeName 'System.Management.Automation.Language.ScriptExtent' -ArgumentList $innerExtentStartPos,$innerExtentEndPos
+            $extentGetter = {
+                param($line, $startLineNum, $startColumnNum, $endLineNum, $endColumnNum)
+                $extentStartPos = New-Object -TypeName $scriptPositionType -ArgumentList $null, $startLineNum, $startColumnNum, $line
+                $extentEndPos = New-Object -TypeName $scriptPositionType -ArgumentList $null, $endLineNum, $endColumnNum, $line
+                New-Object -TypeName $scriptExtentType -ArgumentList $extentStartPos, $extentEndPos
+            }
 
-           [Microsoft.Windows.PowerShell.ScriptAnalyzer.Extensions.Extensions]::Contains($outerExtent, $innerExtent) | Should Be $true
+            $outerExtent = & $extentGetter $outer 1 1 1 ($outer.Length + 1)
+        }
+
+        It "Should return true if the inner extent is strictly contained in the outer extent on the begin boundary" {
+            $innerExtent = & $extentGetter $outer 1 1 1 2
+            [Microsoft.Windows.PowerShell.ScriptAnalyzer.Extensions.Extensions]::Contains($outerExtent, $innerExtent) | Should Be $true
+        }
+
+        It "Should return true if the inner extent is strictly contained in the outer extent on the end boundary" {
+            $innerExtent = & $extentGetter $outer 1 23 1 25
+            [Microsoft.Windows.PowerShell.ScriptAnalyzer.Extensions.Extensions]::Contains($outerExtent, $innerExtent) | Should Be $true
+        }
+
+        It "Should return false if the inner extent is partially contained in the outer extent" {
+            $innerExtent = & $extentGetter $outer 1 23 1 26
+            [Microsoft.Windows.PowerShell.ScriptAnalyzer.Extensions.Extensions]::Contains($outerExtent, $innerExtent) | Should Be $false
+        }
+
+        It "Should return false if the inner extent is strictly not contained in the outer extent" {
+            $innerExtent = & $extentGetter $outer 1 25 1 27
+            [Microsoft.Windows.PowerShell.ScriptAnalyzer.Extensions.Extensions]::Contains($outerExtent, $innerExtent) | Should Be $false
         }
     }
 }
