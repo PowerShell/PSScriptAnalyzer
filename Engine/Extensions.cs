cbf182b753c8d670b2e88303a52e2d99053f2837
Kapil Borle <kborle@microsoft.com>
2017-04-28 14:39:24 -0700

    Add extension method to translate text coordinates

--- Engine/Extensions.cs
+++ Engine/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Management.Automation.Language;
 
@@ -20,6 +21,36 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.Extensions
                     line = stringReader.ReadLine();
                 }
             }
+        }
+
+        public static IScriptExtent Translate(this IScriptExtent extent, int lineDelta, int columnDelta)
+        {
+            var newStartLineNumber = extent.StartLineNumber + lineDelta;
+            if (newStartLineNumber < 1)
+            {
+                throw new ArgumentException(
+                    "Invalid line delta. Resulting start line number must be greather than 1.");
+            }
+
+            var newStartColumnNumber = extent.StartColumnNumber + columnDelta;
+            var newEndColumnNumber = extent.EndColumnNumber + columnDelta;
+            if (newStartColumnNumber < 1 || newEndColumnNumber < 1)
+            {
+                throw new ArgumentException(@"Invalid column delta.
+Resulting start column and end column number must be greather than 1.");
+            }
+
+            return new ScriptExtent(
+                new ScriptPosition(
+                    extent.File,
+                    newStartLineNumber,
+                    newStartColumnNumber,
+                    extent.StartScriptPosition.Line),
+                new ScriptPosition(
+                    extent.File,
+                    extent.EndLineNumber + lineDelta,
+                    newEndColumnNumber,
+                    extent.EndScriptPosition.Line));
         }
     }
 }
