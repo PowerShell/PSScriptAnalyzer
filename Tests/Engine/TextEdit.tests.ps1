bce65630374d499b68579e417d5f065ea51e784f
Kapil Borle <kborle@microsoft.com>
2017-04-27 19:22:57 -0700

    Add tests for TextEdit class

---
+++ Tests/Engine/TextEdit.tests.ps1
@@ -0,0 +1,28 @@
+Import-Module PSScriptAnalyzer
+
+Describe "TextEdit Class" {
+    $type = [Microsoft.Windows.PowerShell.ScriptAnalyzer.TextEdit]
+
+    Context "Object construction" {
+        It "creates the object with correct properties" {
+            $correctionExtent = New-Object -TypeName $type -ArgumentList 1, 2, 3, 4, "get-childitem"
+			$correctionExtent.StartLineNumber | Should Be 1
+            $correctionExtent.EndLineNumber | Should Be 3
+            $correctionExtent.StartColumnNumber | Should Be 2
+            $correctionExtent.EndColumnNumber | Should be 4
+            $correctionExtent.Text | Should Be "get-childitem"
+        }
+
+        It "throws if end line number is less than start line number" {
+            $text = "Get-ChildItem"
+            {New-Object -TypeName $type -ArgumentList @(2, 1, 1, ($text.Length + 1), $text)} |
+                Should Throw "start line number"
+        }
+
+        It "throws if end column number is less than start column number for same line" {
+            $text = "start-process"
+            {New-Object -TypeName $type -ArgumentList @(1, 2, 1, 1, $text)} |
+                    Should Throw "start column number"
+            }
+        }
+    }
