717da67a6af2b0efdb74e67a02b7a0f91e03f6ca
Kapil Borle <kborle@microsoft.com>
2017-05-03 19:06:49 -0700

    Derive UseSupportsShouldProcess from IScriptRule

--- rules/UseSupportsShouldProcess.cs
+++ rules/UseSupportsShouldProcess.cs
@@ -28,7 +28,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
 #if !CORECLR
     [Export(typeof(IScriptRule))]
 #endif
-    class UseSupportsShouldProcess : ConfigurableRule
+    class UseSupportsShouldProcess : IScriptRule
     {
         private const char whitespace = ' ';
         private const int indentationSize = 4;
@@ -42,7 +42,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <param name="ast">AST to be analyzed. This should be non-null</param>
         /// <param name="fileName">Name of file that corresponds to the input AST.</param>
         /// <returns>A an enumerable type containing the violations</returns>
-        public override IEnumerable<DiagnosticRecord> AnalyzeScript(Ast ast, string fileName)
+        public IEnumerable<DiagnosticRecord> AnalyzeScript(Ast ast, string fileName)
         {
             if (ast == null)
             {
@@ -466,7 +466,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <summary>
         /// Retrieves the common name of this rule.
         /// </summary>
-        public override string GetCommonName()
+        public string GetCommonName()
         {
             return string.Format(CultureInfo.CurrentCulture, Strings.UseSupportsShouldProcessCommonName);
         }
@@ -474,7 +474,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <summary>
         /// Retrieves the description of this rule.
         /// </summary>
-        public override string GetDescription()
+        public string GetDescription()
         {
             return string.Format(CultureInfo.CurrentCulture, Strings.UseSupportsShouldProcessDescription);
         }
@@ -482,7 +482,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <summary>
         /// Retrieves the name of this rule.
         /// </summary>
-        public override string GetName()
+        public string GetName()
         {
             return string.Format(
                 CultureInfo.CurrentCulture,
@@ -494,7 +494,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <summary>
         /// Retrieves the name of the module/assembly the rule is from.
         /// </summary>
-        public override string GetSourceName()
+        public string GetSourceName()
         {
             return string.Format(CultureInfo.CurrentCulture, Strings.SourceName);
         }
@@ -502,7 +502,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <summary>
         /// Retrieves the type of the rule, Builtin, Managed or Module.
         /// </summary>
-        public override SourceType GetSourceType()
+        public SourceType GetSourceType()
         {
             return SourceType.Builtin;
         }
@@ -510,7 +510,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <summary>
         /// Retrieves the severity of the rule: error, warning or information.
         /// </summary>
-        public override RuleSeverity GetSeverity()
+        public RuleSeverity GetSeverity()
         {
             return RuleSeverity.Warning;
         }
