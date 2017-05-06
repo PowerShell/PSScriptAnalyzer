5d0edd323ef1e263b42eb6ea942d7ee4802d07d8
Kapil Borle <kborle@microsoft.com>
2017-05-06 13:43:21 -0700

    Remove IScriptExtent.Translate method from extensions

--- Engine/Extensions.cs
+++ Engine/Extensions.cs
@@ -13,36 +13,6 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.Extensions
             return text.Split('\n').Select(line => line.TrimEnd('\r'));
         }
 
-        public static IScriptExtent Translate(this IScriptExtent extent, int lineDelta, int columnDelta)
-        {
-            var newStartLineNumber = extent.StartLineNumber + lineDelta;
-            if (newStartLineNumber < 1)
-            {
-                throw new ArgumentException(
-                    "Invalid line delta. Resulting start line number must be greather than 1.");
-            }
-
-            var newStartColumnNumber = extent.StartColumnNumber + columnDelta;
-            var newEndColumnNumber = extent.EndColumnNumber + columnDelta;
-            if (newStartColumnNumber < 1 || newEndColumnNumber < 1)
-            {
-                throw new ArgumentException(@"Invalid column delta.
-Resulting start column and end column number must be greather than 1.");
-            }
-
-            return new ScriptExtent(
-                new ScriptPosition(
-                    extent.File,
-                    newStartLineNumber,
-                    newStartColumnNumber,
-                    extent.StartScriptPosition.Line),
-                new ScriptPosition(
-                    extent.File,
-                    extent.EndLineNumber + lineDelta,
-                    newEndColumnNumber,
-                    extent.EndScriptPosition.Line));
-        }
-
         /// <summary>
         /// Converts IScriptExtent to Range
         /// </summary>
