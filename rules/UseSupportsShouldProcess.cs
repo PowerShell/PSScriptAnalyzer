1f2f6af4bdef30e2a08b54991dff587533318f14
Kapil Borle <kborle@microsoft.com>
2017-05-17 20:09:58 -0700

    Fix NRE in UseSupportsShouldProcess rule

--- rules/UseSupportsShouldProcess.cs
+++ rules/UseSupportsShouldProcess.cs
@@ -65,7 +65,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
                 int whatIfIndex, confirmIndex;
                 ParameterAst whatIfParamAst, confirmParamAst;
                 ParamBlockAst paramBlockAst;
-                var parameterAsts = functionDefinitionAst.GetParameterAsts(out paramBlockAst).ToArray();
+                var parameterAsts = functionDefinitionAst.GetParameterAsts(out paramBlockAst)?.ToArray();
                 if (parameterAsts == null)
                 {
                     continue;
