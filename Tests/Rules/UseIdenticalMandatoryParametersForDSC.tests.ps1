4f73aa880a50267d6a08d0a2e8e2d5626367da39
Kapil Borle <kborle@microsoft.com>
2017-06-02 18:52:19 -0700

    Add tests for updated logic

--- Tests/Rules/UseIdenticalMandatoryParametersForDSC.tests.ps1
+++ Tests/Rules/UseIdenticalMandatoryParametersForDSC.tests.ps1
@@ -1,26 +1,37 @@
 Import-Module PSScriptAnalyzer
 $directory = Split-Path -Parent $MyInvocation.MyCommand.Path
 $ruleName = 'PSDSCUseIdenticalMandatoryParametersForDSC'
-$resourceFilepath = [System.IO.Path]::Combine(
-    $directory,
-    'DSCResourceModule',
-    'DSCResources',
+$resourceBasepath = "$directory\DSCResourceModule\DSCResources"
+$badResourceFilepath = [System.IO.Path]::Combine(
+    $resourceBasepath,
     'MSFT_WaitForAnyNoIdenticalMandatoryParameter',
     'MSFT_WaitForAnyNoIdenticalMandatoryParameter.psm1');
+$goodResourceFilepath = [System.IO.Path]::Combine($resourceBasepath,'MSFT_WaitForAny','MSFT_WaitForAny.psm1');
+
 
 Describe "UseIdenticalMandatoryParametersForDSC" {
-    Context "When a mandatory parameter is not present" {
+    Context "When a mandatory parameters are not present" {
         BeforeAll {
-            $violations = Invoke-ScriptAnalyzer -Path $resourceFilepath -IncludeRule $ruleName
+            $violations = Invoke-ScriptAnalyzer -Path $badResourceFilepath -IncludeRule $ruleName
         }
 
-        # todo add a test to check one violation per function
         It "Should find a violations" {
-            $violations.Count | Should Be 1
+            $violations.Count | Should Be 4
         }
 
         It "Should mark only the function name" {
             $violations[0].Extent.Text | Should Be 'Get-TargetResource'
+        }
+    }
+
+    Context "When all mandatory parameters are present" {
+        BeforeAll {
+            $violations = Invoke-ScriptAnalyzer -Path $goodResourceFilepath -IncludeRule $ruleName
+        }
+
+        # todo add a test to check one violation per function
+        It "Should find a violations" {
+            $violations.Count | Should Be 0
         }
     }
 }
