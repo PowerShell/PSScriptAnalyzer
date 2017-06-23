cf01c041db3002dc57146abd933aee2afccf600e
Kapil Borle <kborle@microsoft.com>
2017-06-23 11:57:06 -0700

    Add restore task

---
+++ .build.ps1
@@ -0,0 +1,14 @@
+param(
+    [ValidateSet("net451", "netstandard1.6")]
+    [string]$Framework = "netstandard1.6",
+
+    [ValidateSet("Debug", "Release", "PSv3Debug", "PSv3Release")]
+    [string]$Configuration = "Debug"
+)
+
+task -Name restore `
+    -Inputs Engine/project.json, Rules/project.json `
+    -Outputs Engine/project.lock.json, Rules/project.lock.json `
+    -Jobs {
+    dotnet restore
+}
