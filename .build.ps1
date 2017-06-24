04c36f58373b049189ff661cc2bd4b9242c690d4
Kapil Borle <kborle@microsoft.com>
2017-06-23 17:45:21 -0700

    Add build, restore and clean tasks

--- .build.ps1
+++ .build.ps1
@@ -6,9 +6,80 @@ param(
     [string]$Configuration = "Debug"
 )
 
-task -Name restore `
-    -Inputs Engine/project.json, Rules/project.json `
-    -Outputs Engine/project.lock.json, Rules/project.lock.json `
-    -Jobs {
-    dotnet restore
+function Get-BuildInputs($project) {
+    pushd $buildRoot/$project
+    gci -Filter *.cs
+    gci -Directory -Exclude obj,bin | gci -Filter *.cs -Recurse
+    popd
 }
+
+function Get-BuildOutputs($project) {
+    $bin = "$buildRoot/$project/bin/$Configuration/$Framework"
+    $obj = "$buildRoot/$project/obj/$Configuration/$Framework"
+    if (Test-Path $bin) {
+        gci $bin -Recurse
+    }
+    if (Test-Path $obj) {
+        gci $obj -Recurse
+    }
+}
+
+function Get-BuildTaskParams($project) {
+    $taskParams = @{
+        Jobs    = {dotnet build --framework $Framework --configuration $Configuration}
+    }
+
+    $outputs = (Get-BuildOutputs $project)
+    if ($null -ne $outputs) {
+        $inputs = (Get-BuildInputs $project)
+        $taskParams.Add("Outputs", $outputs)
+        $taskParams.Add("Inputs", $inputs)
+    }
+
+    $taskParams
+}
+
+function Get-RestoreTaskParams($project) {
+    @{
+        Inputs = "$BuildRoot/$project/project.json"
+        Outputs = "$BuildRoot/$project/project.lock.json"
+        Jobs = {dotnet restore}
+    }
+}
+
+function Get-CleanTaskParams($project) {
+    @{
+        Jobs = {
+            if (Test-Path obj) {
+                Remove-Item obj -Force -Recurse
+            }
+
+            if (Test-Path bin) {
+                Remove-Item bin -Force -Recurse
+            }
+        }
+    }
+}
+
+function Add-ProjectTask([string]$project, [string]$taskName, [hashtable]$taskParams) {
+   $jobs = [scriptblock]::Create(@"
+pushd $buildRoot/$project
+$($taskParams.Jobs)
+popd
+"@)
+    $taskParams.Jobs = $jobs
+    $taskParams.Name = "$project/$taskName"
+    task @taskParams
+}
+
+$projects = @("engine", "rules")
+
+$projects | % {Add-ProjectTask $_ build (Get-BuildTaskParams $_)}
+task build "engine/build", "rules/build"
+
+$projects | % {Add-ProjectTask $_ "restore" (Get-RestoreTaskParams $_)}
+task restore "engine/restore", "rules/restore"
+
+$projects | % {Add-ProjectTask $_ clean (Get-CleanTaskParams $_)}
+task clean "engine/clean", "rules/clean"
+
