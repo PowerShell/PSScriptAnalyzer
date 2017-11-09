a16c232f6b4c8dc168bd6e0caf757a2a5fc0183f
bergmeister <c.bergmeister@gmail.com>
2017-11-09 05:56:11 +0000

    Always return true for shouldprocess

--- Tests/Engine/LibraryUsage.tests.ps1
+++ Tests/Engine/LibraryUsage.tests.ps1
@@ -78,7 +78,7 @@ function Invoke-ScriptAnalyzer {
 	);
 
     if ($PSCmdlet.ParameterSetName -eq "File") {
-		$supportsShouldProcessFunc = [Func[string, string, bool]]{ return $Recurse.IsPresent }
+		$supportsShouldProcessFunc = [Func[string, string, bool]]{ return $true }
 		if ($Fix.IsPresent)
 		{
 			return $scriptAnalyzer.AnalyzeAndFixPath($Path, $supportsShouldProcessFunc, $Recurse.IsPresent);
