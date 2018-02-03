c4c8c6e3576542aa2c536fc7a7bb3ab2d04168c9
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-02-03 17:27:30 +0000

    add first tests

---
+++ Tests/Rules/AvoidAssignmentToAutomaticVariable.tests.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module PSScriptAnalyzer
+$ruleName = "PSAvoidAssignmentToAutomaticVariable"
+
+Describe "AvoidAssignmentToAutomaticVariables" {
+    Context "ReadOnly Variables" {
+        It "'?' Variable" {
+            $warnings = Invoke-ScriptAnalyzer -ScriptDefinition '$? = Get-Alias' | Where-Object { $_.RuleName -eq $ruleName }
+            $warnings.Count | Should Be 1
+        }
+
+        It "True Variable" {
+            $warnings = Invoke-ScriptAnalyzer -ScriptDefinition '$true = Get-Alias' | Where-Object { $_.RuleName -eq $ruleName }
+            $warnings.Count | Should Be 1
+        }
+    }
+}
