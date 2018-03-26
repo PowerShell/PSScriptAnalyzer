31055fd0f659867b05fa067e5c0c1f0b306368d4
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-03-26 22:18:33 +0100

    change test syntax as requested in PR

--- Tests/Rules/AvoidUsingAlias.tests.ps1
+++ Tests/Rules/AvoidUsingAlias.tests.ps1
@@ -101,7 +101,7 @@ Configuration MyDscConfiguration {
             $violations.Count | Should -Be 0
         }
 
-        It "do not warn when about Get-* completed cmdlets when the command exists natively on Unix platforms" -skip:(!$IsLinux -and !$IsmacOS) {
+        It "do not warn when about Get-* completed cmdlets when the command exists natively on Unix platforms" -skip:(-not ($IsLinux -or $IsMacOS)) {
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition 'date' | Where-Object { $_.RuleName -eq $violationName }
             $violations.Count | Should -Be 0
         }
