e4dc844d367bcc7e9cdf38e144cd92d435f34415
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-03-31 18:36:51 +0100

    fix tests to not run in library usage session

--- Tests/Engine/InvokeScriptAnalyzer.tests.ps1
+++ Tests/Engine/InvokeScriptAnalyzer.tests.ps1
@@ -563,24 +563,26 @@ Describe "Test -EnableExit Switch" {
         }
     }
 
-    Describe "Handles parse errors due to unknown types" {
-        $script = @'
-        using namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels
-        using namespace Microsoft.Azure.Commands.Common.Authentication.Abstractions
-        Import-Module "AzureRm"
-        class MyClass { [IStorageContext]$StorageContext } # This will result in a parser error due to [IStorageContext] type that comes from the using stetement but is not known at parse time
-        gci # Produce AvoidAlias rule
+    if (!$testingLibraryUsage) {
+        Describe "Handles parse errors due to unknown types" {
+            $script = @'
+                using namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels
+                using namespace Microsoft.Azure.Commands.Common.Authentication.Abstractions
+                Import-Module "AzureRm"
+                class MyClass { [IStorageContext]$StorageContext } # This will result in a parser error due to [IStorageContext] type that comes from the using stetement but is not known at parse time
+                gci # Produce AvoidAlias rule
 '@
-        It "does not throw and detect one expected warning after the parse error has occured when using -ScriptDefintion parameter set" {
-            $warnings = Invoke-ScriptAnalyzer -ScriptDefinition $script
-            $warnings.Count | Should -Be 1 -Because 'PSSA should analyze the whole script after the parse error on [IStorageContext] and find the AvoidAlias warning due to gci'
-        }
+            It "does not throw and detect one expected warning after the parse error has occured when using -ScriptDefintion parameter set" {
+                $warnings = Invoke-ScriptAnalyzer -ScriptDefinition $script
+                $warnings.Count | Should -Be 1 -Because 'PSSA should analyze the whole script after the parse error on [IStorageContext] and find the AvoidAlias warning due to gci'
+            }
 
-        $testFilePath = "TestDrive:\testfile.ps1"
-        Set-Content $testFilePath -value $script
-        It "does not throw and detect one expected warning after the parse error has occured when using -Path parameter set" {
-            $warnings = Invoke-ScriptAnalyzer -Path $testFilePath
-            $warnings.Count | Should -Be 1 -Because 'PSSA should analyze the whole script after the parse error on [IStorageContext] and find the AvoidAlias warning due to gci'
+            $testFilePath = "TestDrive:\testfile.ps1"
+            Set-Content $testFilePath -value $script
+            It "does not throw and detect one expected warning after the parse error has occured when using -Path parameter set" {
+                $warnings = Invoke-ScriptAnalyzer -Path $testFilePath
+                $warnings.Count | Should -Be 1 -Because 'PSSA should analyze the whole script after the parse error on [IStorageContext] and find the AvoidAlias warning due to gci'
+            }
         }
     }
 }
