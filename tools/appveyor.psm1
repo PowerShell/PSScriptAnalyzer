c2c12e50153c5d0349b66feac18d5ef199494d98
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-04-07 18:34:42 +0100

    another similar test fix (same as before) and restore erroractionpreference in module again

--- tools/appveyor.psm1
+++ tools/appveyor.psm1
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+$ErrorActionPreference = 'Stop'
+
 # Implements the AppVeyor 'install' step and installs the required versions of Pester, platyPS and the .Net Core SDK if needed.
 function Invoke-AppVeyorInstall {
     $requiredPesterVersion = '4.3.1'
