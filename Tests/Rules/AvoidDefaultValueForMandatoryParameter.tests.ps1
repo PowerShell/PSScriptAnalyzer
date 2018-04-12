a49cdafb3b5b635fe8ecb74d7818000d73961167
kalgiz <klalgiz@microsoft.com>
2018-04-12 15:57:29 -0700

    Add tests for AvoidDefaultValueForMandatoryParameter rule.

--- Tests/Rules/AvoidDefaultValueForMandatoryParameter.tests.ps1
+++ Tests/Rules/AvoidDefaultValueForMandatoryParameter.tests.ps1
@@ -13,10 +13,22 @@ Describe "AvoidDefaultValueForMandatoryParameter" {
                 Where-Object { $_.RuleName -eq $ruleName }
             $violations.Count | Should -Be 1
         }
+
+        It "returns violations when the parameter is specified as mandatory=1 and has a default value" {
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition 'Function foo{ Param([Parameter(Mandatory=1)]$Param1=''defaultValue'') }' |
+                Where-Object { $_.RuleName -eq $ruleName }
+            $violations.Count | Should -Be 1
+        }
     }
 
     Context "When there are no violations" {
         It "has 1 provide default value for mandatory parameter violation" {
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition 'Function foo{ Param([Parameter(Mandatory=$false)]$Param1=''val1'', [Parameter(Mandatory)]$Param2=''val2'', $Param3=''val3'') }' |
+                Where-Object { $_.RuleName -eq $ruleName }
+            $violations.Count | Should -Be 0
+        }
+
+        It "returns no violations when the parameter is specified as mandatory=0 and has a default value" {
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition 'Function foo{ Param([Parameter(Mandatory=$false)]$Param1=''val1'', [Parameter(Mandatory)]$Param2=''val2'', $Param3=''val3'') }' |
                 Where-Object { $_.RuleName -eq $ruleName }
             $violations.Count | Should -Be 0
