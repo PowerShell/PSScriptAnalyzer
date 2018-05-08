82ebc03d9731f9117869714014db22b7a651dc08
James Truher <jimtru@microsoft.com>
2018-05-08 14:13:55 -0700

    Change vsts build script to be able to put built module anywhere based on _DockerVolume_
    Remove a comment

--- tools/releaseBuild/Image/buildPSSA.ps1
+++ tools/releaseBuild/Image/buildPSSA.ps1
@@ -1,4 +1,5 @@
+param ( [string]$target )
 push-location C:/PSScriptAnalyzer
 import-module C:/PSScriptAnalyzer/Utils/ReleaseMaker.psm1
 New-ReleaseBuild
-Copy-Item -Recurse C:/PSScriptAnalyzer/out C:/
+Copy-Item -Recurse C:/PSScriptAnalyzer/out/* ${target}
