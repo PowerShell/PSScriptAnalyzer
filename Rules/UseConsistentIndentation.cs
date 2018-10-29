9acf387b33147000e30753af84befdcfaabe28be
Pedro Amaral <36695843+pedrocodacy@users.noreply.github.com>
2018-10-29 20:38:29 +0100

    Add summary comment to some rules with short description (#1087)
    * Add summary comment to some rules with short description
    
    * correct typo
    
    * add period
    
    * add period

--- Rules/UseConsistentIndentation.cs
+++ Rules/UseConsistentIndentation.cs
@@ -14,7 +14,7 @@ using Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic;
 namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
 {
     /// <summary>
-    /// A class to walk an AST to check for violation.
+    /// UseConsistentIndentation: Checks if indentation is consistent throughout the source file.
     /// </summary>
 #if !CORECLR
     [Export(typeof(IScriptRule))]
