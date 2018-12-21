42c54b026e6a6bccde7956233ab84141b79e3b88
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-12-21 16:22:03 +0000

    fix tests

--- Tests/Rules/UseCorrectCasing.tests.ps1
+++ Tests/Rules/UseCorrectCasing.tests.ps1
@@ -17,6 +17,6 @@ Describe "UseCorrectCasing" {
 
         }
         Invoke-Formatter 'invoke-dummyFunction' | Should -Be 'Invoke-DummyFunction'
-        Invoke-ScriptAnalyzer 'foo' # Workaround for this bug@ https://github.com/PowerShell/PSScriptAnalyzer/issues/1116
+        Invoke-ScriptAnalyzer -ScriptDefinition 'foo' # Workaround for this bug https://github.com/PowerShell/PSScriptAnalyzer/issues/1116
     }
 }
