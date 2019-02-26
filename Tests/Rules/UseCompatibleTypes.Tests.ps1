3a3e0a8cbf6a95862877c01449f25106d9d1bf38
Robert Holt <roholt@microsoft.com>
2019-02-26 10:58:08 -0800

    Fix type compat test profile bugs

--- Tests/Rules/UseCompatibleTypes.Tests.ps1
+++ Tests/Rules/UseCompatibleTypes.Tests.ps1
@@ -149,9 +149,12 @@ Describe 'UseCompatibleTypes' {
                         $script:TargetProfileConfigKey = @(
                             $script:Srv2012_3_profile
                             $script:Srv2012r2_4_profile
-                            $script:Srv2012r2_6_1_profile
+                            $script:Srv2016_5_profile
+                            $script:Srv2016_6_1_profile
                             $script:Srv2019_5_profile
                             $script:Srv2019_6_1_profile
+                            $script:Win10_5_profile
+                            $script:Win10_6_1_profile
                             $script:Ubuntu1804_6_1_profile
                         )
                         IgnoreTypes = @('System.IO.Compression.ZipFile')
