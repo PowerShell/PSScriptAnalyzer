cb22eec1608dcc521aa4f4080922876a35194773
Robert Holt <rjmholt_msft@outlook.com>
2019-02-27 06:30:41 -0800

    Add verbose message to release script

--- Utils/ReleaseMaker.psm1
+++ Utils/ReleaseMaker.psm1
@@ -94,8 +94,9 @@ function New-ReleaseBuild
     {
         if ( test-path out ) { remove-item out/ -recurse -force }
         .\build.ps1 -All -Configuration Release
-        .\PSCompatibilityAnalyzer\build.ps1 -Clean
+        .\PSCompatibilityAnalyzer\build.ps1 -Clean -Configuration Release
         Copy-Item -Recurse .\PSCompatibilityAnalyzer\out\* .\out\
+        Write-Verbose (Get-ChildItem ./out)
     }
     finally
     {
