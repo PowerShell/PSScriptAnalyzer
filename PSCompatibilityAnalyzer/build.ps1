5fb1e86ca453520b6f27e157d747cc1d3ed7e3e9
Robert Holt <rjmholt_msft@outlook.com>
2019-02-27 05:46:23 -0800

    Fix build name bug

--- PSCompatibilityAnalyzer/build.ps1
+++ PSCompatibilityAnalyzer/build.ps1
@@ -32,7 +32,7 @@ $script:Psd1Path = [System.IO.Path]::Combine($PSScriptRoot, 'PSCompatibilityAnal
 $script:ProfileDirPath = [System.IO.Path]::Combine($PSScriptRoot, 'profiles')
 
 $script:BinModDir = [System.IO.Path]::Combine($PSScriptRoot, 'out', 'PSCompatibilityAnalyzer')
-$script:BinModSrcDir = Join-Path $PSScriptRoot 'Microsoft.PowerShell.PSCompatibilityAnalyzer'
+$script:BinModSrcDir = Join-Path $PSScriptRoot 'Microsoft.PowerShell.CrossCompatibility'
 
 $script:PublishDlls = @{
     'net452' = @('Microsoft.PowerShell.CrossCompatibility.dll', 'Microsoft.PowerShell.CrossCompatibility.pdb', 'Newtonsoft.Json.dll')
