88a6a1d51086dd0e72fc664f4afb7dc7339d5565
Robert Holt <rjmholt_msft@outlook.com>
2019-04-09 19:48:48 -0700

    Change test implementation to out-of-proc, add more printing

--- PSCompatibilityCollector/Tests/Cmdlet.Tests.ps1
+++ PSCompatibilityCollector/Tests/Cmdlet.Tests.ps1
@@ -1,72 +1,75 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-Write-Verbose -Verbose (Get-Module -ListAvailable | Out-String)
+Write-Verbose -Verbose ("Modules:`n" + (Get-Module -ListAvailable | Out-String))
+Write-Verbose -Verbose ("Exported cmdlets from PSCompatibilityCollector`n:" + (Get-Module -ListAvailable PSCompatibilityCollector | % { $_.ExportedCmdlets.Keys } | Out-String))
 
 $script:currentMachineProfile = New-PSCompatibilityProfile -PassThru
 
-Describe "Assert-PSCompatibilityProfileIsValid" {
-    It "Accepts a good profile" {
-        Assert-PSCompatibilityProfileIsValid -CompatibilityProfile $script:currentMachineProfile
-        $? | Should -BeTrue
+Describe "PSCompatiblityCollector cmdlets" {
+    BeforeAll {
+        $compatModulePath = (Get-Module -ListAvailable 'PSCompatibilityCollector')[0].Path
+        $pwshName = (Get-Process -Id $PID).Path
+        $profileLocation = Join-Path $TestDrive 'profile.json'
+
+        & $pwshName -Command "Import-Module '$compatModulePath'; New-PSCompatibilityProfile -OutFile '$profileLocation'"
+
+        $currentMachineProfile = ConvertFrom-PSCompatibilityJson -Path $profileLocation
     }
-}
 
-Describe "JSON cmdlets" {
-    It "Serializes a compatibility profile to JSON and back again" {
-        $script:currentMachineProfile |
-            ConvertTo-PSCompatibilityJson |
-            ConvertFrom-PSCompatibilityJson |
-            Assert-PSCompatibilityProfileIsValid
-
-        $? | Should -BeTrue
-    }
-}
-
-Describe "Platform data and naming" {
-    It "Collects platform data properly" {
-        $platformData = Get-PSCompatibilityPlatformData
-
-        $platformData.PowerShell.Version.Major | Should -Be $PSVersionTable.PSVersion.Major
-
-        if ($PSEdition -eq 'Core')
-        {
-            $platformData.Dotnet.Edition | Should -Be 'Core'
-        }
-        else
-        {
-            $platformData.Dotnet.Edition | Should -Be 'Framework'
-        }
-
-        if ($IsLinux)
-        {
-            $platformData.OperatingSystem.Family | Should -Be 'Linux'
-        }
-        elseif ($IsMacOS)
-        {
-            $platformData.OperatingSystem.Family | Should -Be 'MacOS'
-        }
-        else
-        {
-            $platformData.OperatingSystem.Family | Should -Be 'Windows'
+    Context "Assert-PSCompatibilityProfileIsValid" {
+        It "Accepts a good profile" {
+            Assert-PSCompatibilityProfileIsValid -CompatibilityProfile $currentMachineProfile
+            $? | Should -BeTrue
         }
     }
 
-    It "Names the platform appropriately" {
-        $platformName = Get-PSCompatibilityPlatformName
+    Context "JSON cmdlets" {
+        It "Serializes a compatibility profile to JSON and back again" {
+            $currentMachineProfile |
+                ConvertTo-PSCompatibilityJson |
+                ConvertFrom-PSCompatibilityJson |
+                Assert-PSCompatibilityProfileIsValid
 
-        $platformNameElements = $platformName -split "_"
-
-        $platformNameElements | Should -HaveCount 6
+            $? | Should -BeTrue
+        }
     }
-}
 
-Describe "New-PSCompatibilityProfile" {
-    It "Generates a compatibility profile" {
-        $jsonPath = Join-Path $TestDrive "profile.json"
-        New-PSCompatibilityProfile -OutFile $jsonPath
-        $json = ConvertFrom-PSCompatibilityJson -Path $jsonPath
-        Assert-PSCompatibilityProfileIsValid $json
-        $? | Should -BeTrue
+    Context "Platform data and naming" {
+        It "Collects platform data properly" {
+            $platformData = Get-PSCompatibilityPlatformData
+
+            $platformData.PowerShell.Version.Major | Should -Be $PSVersionTable.PSVersion.Major
+
+            if ($PSEdition -eq 'Core')
+            {
+                $platformData.Dotnet.Edition | Should -Be 'Core'
+            }
+            else
+            {
+                $platformData.Dotnet.Edition | Should -Be 'Framework'
+            }
+
+            if ($IsLinux)
+            {
+                $platformData.OperatingSystem.Family | Should -Be 'Linux'
+            }
+            elseif ($IsMacOS)
+            {
+                $platformData.OperatingSystem.Family | Should -Be 'MacOS'
+            }
+            else
+            {
+                $platformData.OperatingSystem.Family | Should -Be 'Windows'
+            }
+        }
+
+        It "Names the platform appropriately" {
+            $platformName = Get-PSCompatibilityPlatformName
+
+            $platformNameElements = $platformName -split "_"
+
+            $platformNameElements | Should -HaveCount 6
+        }
     }
 }
