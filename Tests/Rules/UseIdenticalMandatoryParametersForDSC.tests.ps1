9bdb4b290fe49add2c01059d26fb0e88331823f9
Christoph Bergmeister <c.bergmeister@gmail.com>
2019-05-02 21:28:15 +0100

    add test

--- Tests/Rules/UseIdenticalMandatoryParametersForDSC.tests.ps1
+++ Tests/Rules/UseIdenticalMandatoryParametersForDSC.tests.ps1
@@ -83,4 +83,75 @@ class ClassWithNoParent
             $violations.Count | Should -Be 0
         }
     }
+
+    Context "When a CIM class has no parent, but does contain a subclass which should not be processed" {
+        # regression test for #1192 - just check no uncaught exception
+        It "Should find no violations, and throw no exceptions" -skip:($IsLinux -or $IsMacOS) {
+
+            # Arrange test content in testdrive
+            $dscResources = Join-Path -Path "TestDrive:" -ChildPath "DSCResources"
+            $noparentClassDir = Join-Path -Path $dscResources "ClassWithNoParent"
+
+            # need a fake module
+            $fakeModulePath = Join-Path -Path "TestDrive:" -ChildPath "test.psd1"
+            Set-Content -Path $fakeModulePath -Value @"
+@{
+    ModuleVersion = '1.0'
+    GUID = 'f5e6cc2a-5500-4592-bbe2-ef033754b56f'
+    Author = 'test'
+
+    FunctionsToExport = @()
+    CmdletsToExport = @()
+    VariablesToExport = '*'
+    AliasesToExport = @()
+
+    # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+    PrivateData = @{
+        PSData = @{
+        } # End of PSData hashtable
+    } # End of PrivateData hashtable
+}
+"@
+            # and under it a directory called dscresources\something
+            New-Item -ItemType Directory -Path $noParentClassDir
+            $noparentClassFilepath = Join-Path -Path $noParentClassDir -ChildPath 'ClassWithNoParent.psm1'
+            $noparentClassMofFilepath = Join-Path -Path $noParentClassDir -ChildPath 'ClassWithNoParent.schema.mof'
+
+            # containing a .psm1 file and a .schema.mof file with same base name
+            Set-Content -Path $noParentClassFilepath -Value "#requires -Version 4.0 -Modules CimCmdlets" # the file content doesn't much matter
+
+            Set-Content -Path $noParentClassMofFilePath -Value @"
+[ClassVersion("1.0.0.0")]
+Class MSFT_SubClass
+{
+    [Key, Description("Key of the subclass")] String Name;
+    [Required, Description("Required parameter of the subclass")] String Description;
+    [Write, Description("Additional non-required parameter")] Boolean Enabled;
+};
+
+[ClassVersion("1.0.0"), FriendlyName("WaitForAny")]
+class MSFT_WaitForAnyNoIdenticalMandatoryParameter : OMI_BaseResource
+{
+    [key, Description("Name of Resource on remote machine")]
+    string ResourceName;
+
+    [key, Description("dummy variable")]
+    string Dummy;
+
+    [required, Description("List of remote machines")]
+    string NodeName[];
+
+    [required, EmbeddedInstance("MSFT_Credential"), Description("Credential to access all remote machines")]
+    String Credential;
+
+    [write, Description("dummy subclass variable"), EmbeddedInstance("MSFT_SubClass")]
+    string Subclass;
+};
+"@
+
+            # Act - run scriptanalyzer
+            $violations = Invoke-ScriptAnalyzer -Path $noParentClassFilepath -IncludeRule $ruleName -ErrorAction Stop
+            $violations.Count | Should -Be 0
+        }
+    }
 }
