88ab1ec889f69deb0fe2e569ceaf2f0b65595327
Travis C. LaGrone <22419287+travis-c-lagrone@users.noreply.github.com>
2019-06-22 19:26:17 -0500

    Remove `using System.IO`

--- Engine/Settings.cs
+++ Engine/Settings.cs
@@ -7,7 +7,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Language;
