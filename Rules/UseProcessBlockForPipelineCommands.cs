1ef888dbb1720016e9be38f7de94c7995a6e60cc
Matt McNabb <mmcnabb@outlook.com>
2019-11-16 01:29:00 -0500

    Add rule for missing process block #1368

---
+++ Rules/UseProcessBlockForPipelineCommands.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation.Language;
+using Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic;
+#if !CORECLR
+using System.ComponentModel.Composition;
+#endif
+using System.Globalization;
+
+namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
+{
+#if !CORECLR
+    [Export(typeof(IScriptRule))]
+#endif
+    public class UseProcessBlockForPipelineCommands : IScriptRule
+    {
+        public IEnumerable<DiagnosticRecord> AnalyzeScript(Ast ast, string fileName)
+        {
+            if (ast == null) throw new ArgumentNullException(Strings.NullAstErrorMessage);
+
+            IEnumerable<Ast> functionAsts = ast.FindAll(testAst => testAst is FunctionDefinitionAst, true);
+
+            foreach (FunctionDefinitionAst funcAst in functionAsts)
+            {
+                if
+                (
+                    funcAst.Body != null
+                    && funcAst.Body.ParamBlock != null
+                    && funcAst.Body.ParamBlock.Attributes != null
+                    && funcAst.Body.ParamBlock.Parameters != null
+                    && funcAst.Body.ProcessBlock == null
+                )
+                {
+                    foreach (var paramAst in funcAst.Body.ParamBlock.Parameters)
+                    {
+                        foreach (var paramAstAttribute in paramAst.Attributes)
+                        {
+                            if (paramAstAttribute is AttributeAst)
+                            {
+                                var namedArguments = (paramAstAttribute as AttributeAst).NamedArguments;
+                                if (namedArguments != null)
+                                {
+                                    foreach (NamedAttributeArgumentAst namedArgument in namedArguments)
+                                    {
+                                        if
+                                        (
+                                            String.Equals(namedArgument.ArgumentName, "valuefrompipeline", StringComparison.OrdinalIgnoreCase)
+                                            || String.Equals(namedArgument.ArgumentName, "valuefrompipelinebypropertyname", StringComparison.OrdinalIgnoreCase)
+                                        )
+                                        {
+                                            yield return new DiagnosticRecord(
+                                                string.Format(CultureInfo.CurrentCulture, Strings.UseProcessBlockForPipelineCommandsError, paramAst.Name.VariablePath.UserPath),
+                                                paramAst.Name.Extent,
+                                                GetName(),
+                                                DiagnosticSeverity.Warning,
+                                                fileName,
+                                                paramAst.Name.VariablePath.UserPath
+                                            );
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public string GetName()
+        {
+            return string.Format(CultureInfo.CurrentCulture, Strings.NameSpaceFormat, GetSourceName(), Strings.UseProcessBlockForPipelineCommandsName);
+        }
+
+        public string GetCommonName()
+        {
+            return string.Format(CultureInfo.CurrentCulture, Strings.UseProcessBlockForPipelineCommandsCommonName);
+        }
+
+        public string GetDescription()
+        {
+            return string.Format(CultureInfo.CurrentCulture, Strings.UseProcessBlockForPipelineCommandsDescription);
+        }
+
+        public SourceType GetSourceType()
+        {
+            return SourceType.Builtin;
+        }
+
+        public RuleSeverity GetSeverity()
+        {
+            return RuleSeverity.Warning;
+        }
+
+        public string GetSourceName()
+        {
+            return string.Format(CultureInfo.CurrentCulture, Strings.SourceName);
+        }
+    }
+}
