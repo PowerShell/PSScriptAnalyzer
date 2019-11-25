2bd1d84ad79efd06d0c0dc85669ae9e2e019be6e
Igor Iric <iiric@microsfot.com>
2019-11-25 16:35:34 +0100

    re-adding strings

--- Rules/Strings.Designer.cs
+++ Rules/Strings.Designer.cs
@@ -610,6 +610,42 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Avoid Unusable Parameter Name.
+        /// </summary>
+        internal static string AvoidUnusableParameterCommonName {
+            get {
+                return ResourceManager.GetString("AvoidUnusableParameterCommonName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Parameter name should not contain characters which cannot be used in command line. To fix a violation of this rule, please rename parameter to regular name..
+        /// </summary>
+        internal static string AvoidUnusableParameterDescription {
+            get {
+                return ResourceManager.GetString("AvoidUnusableParameterDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Parameter &apos;{0}&apos; has unusable name. To fix a violation of this rule, please rename it..
+        /// </summary>
+        internal static string AvoidUnusableParameterError {
+            get {
+                return ResourceManager.GetString("AvoidUnusableParameterError", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to AvoidUnusableParameter.
+        /// </summary>
+        internal static string AvoidUnusableParameterName {
+            get {
+                return ResourceManager.GetString("AvoidUnusableParameterName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Avoid Using Username and Password Parameters.
         /// </summary>
         internal static string AvoidUsernameAndPasswordParamsCommonName {
