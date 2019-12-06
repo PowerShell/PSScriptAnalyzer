3f1debe3f514a64b4e303e7ac947bd0557d8d30f
James Truher <jimtru@microsoft.com>
2019-12-05 16:05:06 -0800

    Update reference library to include changes in previous hosted analyzer

--- Reference/HostedAnalyzerReference.cs
+++ Reference/HostedAnalyzerReference.cs
@@ -35,20 +35,20 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer {
     public Settings(object settings, System.Func<string, string> presetResolver) { }
     public Settings(object settings) { }
 
-    public System.Collections.Generic.IEnumerable<string> CustomRulePath { get { return default(System.Collections.Generic.IEnumerable<string>); } }
-    public System.Collections.Generic.IEnumerable<string> ExcludeRules { get { return default(System.Collections.Generic.IEnumerable<string>); } }
+    public System.Collections.Generic.List<string> CustomRulePath { get { return default(System.Collections.Generic.List<string>); } }
+    public System.Collections.Generic.List<string> ExcludeRules { get { return default(System.Collections.Generic.List<string>); } }
     public string FilePath { get { return default(string); } }
     public bool IncludeDefaultRules { get { return default(bool); } set { } }
-    public System.Collections.Generic.IEnumerable<string> IncludeRules { get { return default(System.Collections.Generic.IEnumerable<string>); } }
+    public System.Collections.Generic.List<string> IncludeRules { get { return default(System.Collections.Generic.List<string>); } }
     public bool RecurseCustomRulePath { get { return default(bool); } set { } }
     public System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, object>> RuleArguments { get { return default(System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, object>>); } }
-    public System.Collections.Generic.IEnumerable<string> Severities { get { return default(System.Collections.Generic.IEnumerable<string>); } }
-    public void AddRuleArgument ( string name, System.Collections.Generic.Dictionary<string, object> setting ) { }
-    public void AddRuleArgument ( string rule, string setting, object value ) { }
+    public System.Collections.Generic.List<string> Severities { get { return default(System.Collections.Generic.List<string>); } }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings AddRuleArgument ( string name, System.Collections.Generic.Dictionary<string, object> setting ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings AddRuleArgument ( string rule, string setting, object value ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
     public static string GetSettingPresetFilePath ( string settingPreset ) { return default(string); }
     public static System.Collections.Generic.IEnumerable<string> GetSettingPresets (  ) { return default(System.Collections.Generic.IEnumerable<string>); }
     public static string GetShippedSettingsDirectory (  ) { return default(string); }
-    public void SetRuleArgument ( string rule, string setting, object value ) { }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings SetRuleArgument ( string rule, string setting, object value ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
 
   }
 
@@ -156,47 +156,50 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting {
   public class HostedAnalyzer : System.IDisposable {
     public HostedAnalyzer() { }
 
-    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( System.IO.FileInfo File ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
-    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( System.IO.FileInfo File, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( System.Management.Automation.Language.ScriptBlockAst scriptast, System.Management.Automation.Language.Token[] tokens, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings, string filename ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( System.Management.Automation.Language.ScriptBlockAst scriptast, System.Management.Automation.Language.Token[] tokens, string filename ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
     public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( string ScriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
     public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( string ScriptDefinition, System.Collections.Hashtable settings ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
     public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( string ScriptDefinition ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
-    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( System.Management.Automation.Language.ScriptBlockAst scriptast, System.Management.Automation.Language.Token[] tokens, string filename ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
-    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( System.Management.Automation.Language.ScriptBlockAst scriptast, System.Management.Automation.Language.Token[] tokens, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings, string filename ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( System.IO.FileInfo File, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult Analyze ( System.IO.FileInfo File ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult); }
 
-    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( System.IO.FileInfo File ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
-    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( System.IO.FileInfo File, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
     public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( string ScriptDefinition ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
-    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( string ScriptDefinition, System.Collections.Hashtable settings ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
-    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( string ScriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
-    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( System.Management.Automation.Language.ScriptBlockAst scriptast, System.Management.Automation.Language.Token[] tokens, string filename ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
     public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( System.Management.Automation.Language.ScriptBlockAst scriptast, System.Management.Automation.Language.Token[] tokens, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings, string filename ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
+    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( System.Management.Automation.Language.ScriptBlockAst scriptast, System.Management.Automation.Language.Token[] tokens, string filename ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
+    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( string ScriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
+    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( string ScriptDefinition, System.Collections.Hashtable settings ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
+    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( System.IO.FileInfo File, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
+    public System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult> AnalyzeAsync ( System.IO.FileInfo File ) { return default(System.Threading.Tasks.Task<Microsoft.Windows.PowerShell.ScriptAnalyzer.Hosting.AnalyzerResult>); }
 
-
-    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings CreateSettings ( string SettingsName ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
-    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings CreateSettings ( string[] ruleNames ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
     public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings CreateSettings (  ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings CreateSettings ( string[] ruleNames ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
+    public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings CreateSettings ( string SettingsName ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
     public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings CreateSettings ( System.Collections.Hashtable settings ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
     public Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings CreateSettingsFromFile ( string settingsFile ) { return default(Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings); }
 
     public void Dispose (  ) { }
 
-    public string Fix ( string scriptDefinition ) { return default(string); }
     public string Fix ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(string); }
+    public string Fix ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Range range, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(string); }
+    public string Fix ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Range range ) { return default(string); }
+
     public System.Threading.Tasks.Task<string> FixAsync ( string scriptDefinition ) { return default(System.Threading.Tasks.Task<string>); }
+    public System.Threading.Tasks.Task<string> FixAsync ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Range range, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(System.Threading.Tasks.Task<string>); }
     public System.Threading.Tasks.Task<string> FixAsync ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(System.Threading.Tasks.Task<string>); }
 
-    public string Format ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(string); }
     public string Format ( string scriptDefinition ) { return default(string); }
     public string Format ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Range range ) { return default(string); }
+    public string Format ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(string); }
     public string Format ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings, Microsoft.Windows.PowerShell.ScriptAnalyzer.Range range ) { return default(string); }
+
     public System.Threading.Tasks.Task<string> FormatAsync ( string scriptDefinition ) { return default(System.Threading.Tasks.Task<string>); }
     public System.Threading.Tasks.Task<string> FormatAsync ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Range range ) { return default(System.Threading.Tasks.Task<string>); }
     public System.Threading.Tasks.Task<string> FormatAsync ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings ) { return default(System.Threading.Tasks.Task<string>); }
     public System.Threading.Tasks.Task<string> FormatAsync ( string scriptDefinition, Microsoft.Windows.PowerShell.ScriptAnalyzer.Settings settings, Microsoft.Windows.PowerShell.ScriptAnalyzer.Range range ) { return default(System.Threading.Tasks.Task<string>); }
-
     public System.Collections.Generic.List<Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.RuleInfo> GetBuiltinRules ( string[] ruleNames ) { return default(System.Collections.Generic.List<Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.RuleInfo>); }
     public void Reset (  ) { }
+    public override string ToString (  ) { return default(string); }
 
   }
 
