4bc3911f28e3fbaf2e38e1d7c21862c33627fb98
Matt McNabb <mmcnabb@outlook.com>
2019-12-09 13:12:30 -0500

    Add rule for missing process block when command supports the pipeline (#1373)
    * Add rule for missing process block #1368
    
    * Fix build by regenerating strongly typed files by running '.\New-StronglyTypedCsFileForResx.ps1 Rules'
    
    * Update strongly typed resources via Visual Studio
    
    * minimise diff in resx file
    
    * refactor UseProcessBlockForPipelineCommands rule
    
    * add tests and docs for UseProcessBlockForPipelineCommands rule
    
    * Fix logic non-pipeline function and add test case
    
    * fixing some test failures
    
    * incrememt number of expected rules
    
    * fix style suggestions for PR #1373
    
    * fix rule readme.md
    
    * clean diff for Strings.resx to avoid future merge conflicts
    
    * Fix last 2 rule documentation test failures by fixing markdown link
    
    * Update Rules/Strings.resx
    
    Co-Authored-By: Christoph Bergmeister [MVP] <c.bergmeister@gmail.com>
    
    * Update Rules/Strings.resx
    
    Co-Authored-By: Christoph Bergmeister [MVP] <c.bergmeister@gmail.com>
    
    * Update Rules/Strings.resx
    
    Co-Authored-By: Christoph Bergmeister [MVP] <c.bergmeister@gmail.com>
    
    * empty-commit to re-trigger CI due to sporadic failure
    
    * Update Rules/Strings.resx
    
    Co-Authored-By: Robert Holt <rjmholt@gmail.com>
    
    * Update Rules/Strings.resx
    
    Co-Authored-By: Robert Holt <rjmholt@gmail.com>
    
    * corrections to use process block rule and tests - candidate for merge
    
    * More style fixes for #1373
    
    * update localized strings
    
    * replace implicitly typed vars

---
+++ Tests/Rules/UseProcessBlockForPipelineCommand.tests.ps1
@@ -0,0 +1,57 @@
+﻿Describe "UseProcessBlockForPipelineCommand" {
+    BeforeAll {
+        $RuleName = 'PSUseProcessBlockForPipelineCommand'
+    }
+
+    Context "When there are violations" {
+        $Cases = @(
+            @{
+                ScriptDefinition = 'function BadFunc1 { [CmdletBinding()] param ([Parameter(ValueFromPipeline)]$Param1) }'
+                Name = "function without process block"
+            }
+            @{
+                ScriptDefinition = 'function $BadFunc2 { [CmdletBinding()] param ([Parameter(ValueFromPipelineByPropertyName)]$Param1) }'
+                Name = "function without process block by property name"
+            }
+            @{
+                ScriptDefinition = '{ [CmdletBinding()] param ([Parameter(ValueFromPipeline)]$Param1) }'
+                Name = "scriptblock without process block"
+            }
+        )
+        It "has 1 violation for <Name>" -TestCases $Cases {
+            param ($ScriptDefinition)
+
+            Invoke-ScriptAnalyzer -ScriptDefinition $ScriptDefinition -IncludeRule $RuleName | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    Context "When there are no violations" {
+        $Cases = @(
+            @{
+                ScriptDefinition = 'function GoodFunc1 { [CmdletBinding()] param ([Parameter(ValueFromPipeline)]$Param1) process { } }'
+                Name = "function with process block"
+            }
+            @{
+                ScriptDefinition = 'function GoodFunc2 { [CmdletBinding()] param ([Parameter(ValueFromPipelineByPropertyName)]$Param1) process { } }'
+                Name = "function with process block by property name"
+            }
+            @{
+                ScriptDefinition = 'function GoodFunc3 { [CmdletBinding()] param ([Parameter()]$Param1) }'
+                Name = "function without pipeline"
+            }
+            @{
+                ScriptDefinition = 'function GoodFunc3 { [CmdletBinding()] param ([Parameter()][string]$Param1) }'
+                Name = "function with parameter type name"
+            }
+            @{
+                ScriptDefinition = '{ [CmdletBinding()] param ([Parameter(ValueFromPipeline)]$Param1) process { } }'
+                Name = "scriptblock with process block"
+            }
+        )
+        It "has no violations for function <Name>" -TestCases $Cases {
+            param ($ScriptDefinition)
+
+            Invoke-ScriptAnalyzer -ScriptDefinition $ScriptDefinition -IncludeRule $RuleName | Should -BeNullOrEmpty
+        }
+    }
+}
