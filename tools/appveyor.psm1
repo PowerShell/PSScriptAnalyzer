b25714dc434bfb19f83d64bb138473e5654e6fea
Christoph Bergmeister <christoph.bergmeister@bjss.com>
2020-04-12 23:05:56 +0100

    use pester v5 in ci and always upload test results

--- tools/appveyor.psm1
+++ tools/appveyor.psm1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = 'Stop'
 
 function Install-Pester {
-    $requiredPesterVersion = '4.10.0'
+    $requiredPesterVersion = '5.0.0'
     $pester = Get-Module Pester -ListAvailable | Where-Object { $_.Version -eq $requiredPesterVersion }
     if ($null -eq $pester) {
         if ($null -eq (Get-Module -ListAvailable PowershellGet)) {
