51a9fa96549038453d7e683e9543eea622aa2301
Christoph Bergmeister <christoph.bergmeister@bjss.com>
2020-04-13 17:49:17 +0100

    Fix AvoidUsingPlainTextForPassword.tests.ps1

--- Tests/Rules/AvoidUsingPlainTextForPassword.tests.ps1
+++ Tests/Rules/AvoidUsingPlainTextForPassword.tests.ps1
@@ -5,29 +5,28 @@ BeforeAll {
     $violationMessage = [regex]::Escape("Parameter '`$password' should use SecureString, otherwise this will expose sensitive information. See ConvertTo-SecureString for more information.")
     $violationName = "PSAvoidUsingPlainTextForPassword"
     $violationFilepath = Join-Path $PSScriptRoot 'AvoidUsingPlainTextForPassword.ps1'
-    $violations = Invoke-ScriptAnalyzer $violationFilepath | Where-Object {$_.RuleName -eq $violationName}
-    $noViolations = Invoke-ScriptAnalyzer $PSScriptRoot\AvoidUsingPlainTextForPasswordNoViolations.ps1 | Where-Object {$_.RuleName -eq $violationName}
+    $violations = Invoke-ScriptAnalyzer $violationFilepath | Where-Object { $_.RuleName -eq $violationName }
+    $noViolations = Invoke-ScriptAnalyzer $PSScriptRoot\AvoidUsingPlainTextForPasswordNoViolations.ps1 | Where-Object { $_.RuleName -eq $violationName }
     Import-Module (Join-Path $PSScriptRoot "PSScriptAnalyzerTestHelper.psm1")
 }
 
 Describe "AvoidUsingPlainTextForPassword" {
     Context "When there are violations" {
-    $expectedNumViolations = 7
-        It "has $expectedNumViolations violations" {
-            $violations.Count | Should -Be $expectedNumViolations
+        It "has expected violations count" {
+            $violations.Count | Should -Be 7
         }
 
-	It "suggests corrections" {
-	    Test-CorrectionExtent $violationFilepath $violations[0] 1 '$passphrases' '[SecureString] $passphrases'
-	    $violations[0].SuggestedCorrections[0].Description | Should -Be 'Set $passphrases type to SecureString'
+        It "suggests corrections" {
+            Test-CorrectionExtent $violationFilepath $violations[0] 1 '$passphrases' '[SecureString] $passphrases'
+            $violations[0].SuggestedCorrections[0].Description | Should -Be 'Set $passphrases type to SecureString'
 
-	    Test-CorrectionExtent $violationFilepath $violations[1] 1 '$passwordparam' '[SecureString] $passwordparam'
-	    Test-CorrectionExtent $violationFilepath $violations[2] 1 '$credential' '[SecureString] $credential'
-	    Test-CorrectionExtent $violationFilepath $violations[3] 1 '$password' '[SecureString] $password'
-	    Test-CorrectionExtent $violationFilepath $violations[4] 1 '[string]' '[SecureString]'
-	    Test-CorrectionExtent $violationFilepath $violations[5] 1 '[string[]]' '[SecureString[]]'
-	    Test-CorrectionExtent $violationFilepath $violations[6] 1 '[string]' '[SecureString]'
-	}
+            Test-CorrectionExtent $violationFilepath $violations[1] 1 '$passwordparam' '[SecureString] $passwordparam'
+            Test-CorrectionExtent $violationFilepath $violations[2] 1 '$credential' '[SecureString] $credential'
+            Test-CorrectionExtent $violationFilepath $violations[3] 1 '$password' '[SecureString] $password'
+            Test-CorrectionExtent $violationFilepath $violations[4] 1 '[string]' '[SecureString]'
+            Test-CorrectionExtent $violationFilepath $violations[5] 1 '[string[]]' '[SecureString[]]'
+            Test-CorrectionExtent $violationFilepath $violations[6] 1 '[string]' '[SecureString]'
+        }
 
         It "has the correct violation message" {
             $violations[3].Message | Should -Match $violationMessage
