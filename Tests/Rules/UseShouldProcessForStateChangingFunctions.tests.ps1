f2e37f33601fac5797d2b6e15beeb5ee797ec1b8
Christoph Bergmeister <christoph.bergmeister@bjss.com>
2020-04-13 14:06:02 +0100

    Fix UseShouldProcessForStateChangingFunctions.tests.ps1

--- Tests/Rules/UseShouldProcessForStateChangingFunctions.tests.ps1
+++ Tests/Rules/UseShouldProcessForStateChangingFunctions.tests.ps1
@@ -10,26 +10,42 @@ BeforeAll {
 
 Describe "It checks UseShouldProcess is enabled when there are state changing verbs in the function names" {
     Context "When function name has state changing verb" {
-
-        Function Test-Verb([string] $verb)
-        {
-            $scriptDefinitionGeneric = @'
-Function New-{0} () {{ }}
-'@
-            $scriptDef = $scriptDefinitionGeneric -f $verb
-            It ('finds "{0}" verb in function name' -f $verb) {
-                $violations = Invoke-ScriptAnalyzer -ScriptDefinition $scriptDef -IncludeRule $violationName
-                $violations.Count | Should -Be 1
+        It 'Finds verb "<Verb>" in function name' {
+            Param($Verb)
+            $scriptDefinition = "Function New-${Verb} () {{ }}"
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $scriptDefinition -IncludeRule $violationName
+            $violations.Count | Should -Be 1
+        } -TestCases @(
+            @{
+                Verb = 'New'
             }
-        }
-
-        $verbs = @('New', 'Set', 'Remove', 'Start', 'Stop', 'Restart', 'Reset', 'Update')
-        $verbs | ForEach-Object {Test-Verb $_}
+            @{
+                Verb = 'Set'
+            }
+            @{
+                Verb = 'Remove'
+            }
+            @{
+                Verb = 'Start'
+            }
+            @{
+                Verb = 'Stop'
+            }
+            @{
+                Verb = 'Restart'
+            }
+            @{
+                Verb = 'Reset'
+            }
+            @{
+                Verb = 'Update'
+            }
+        )
     }
+
     Context "When there are violations" {
-    	$numViolations = 5
-        It ("has {0} violations where ShouldProcess is not supported" -f $numViolations) {
-            $violations.Count | Should -Be $numViolations
+        It "has correct number of violations where ShouldProcess is not supported" {
+            $violations.Count | Should -Be 5
         }
 
         It "has the correct description message" {
