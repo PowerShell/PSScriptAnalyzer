afb1b986b84b08c0856cc16a02ea8ae09554020c
Christoph Bergmeister <christoph.bergmeister@bjss.com>
2020-04-13 16:57:14 +0100

    Fix ModuleHelp.Tests.ps1 and cleanup

--- Tests/Engine/ModuleHelp.Tests.ps1
+++ Tests/Engine/ModuleHelp.Tests.ps1
@@ -43,21 +43,9 @@ This command runs the tests on the commands in Pester 4.3.1.
 .\Module.Help.Tests.ps1 -ModuleName Pester
 This command runs the tests on the commands in latest local version of the
 Pester module.
-
-
-.NOTES
-	===========================================================================
-	Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.119
-	Created on:   	4/12/2016 1:11 AM
-	Created by:   	June Blender
-	Organization: 	SAPIEN Technologies, Inc
-	Filename:		*.Help.Tests.ps1
-	===========================================================================
 #>
-
 Param
 (
-	[ValidateScript({ Get-Module -ListAvailable -Name $_ })]
 	[string]
 	$ModuleName = 'PSScriptAnalyzer',
 
@@ -65,92 +53,6 @@ Param
 	[System.Version]
 	$RequiredVersion
 )
-
-# #Requires -Module @{ModuleName = 'Pester'; ModuleVersion = '4.3.1'}
-
-<#
-.SYNOPSIS
-Gets command parameters; one per name. Prefers default parameter set.
-
-.DESCRIPTION
-Gets one CommandParameterInfo object for each parameter in the specified
-command. If a command has more than one parameter with the same name, this
-function gets the parameters in the default parameter set, if one is specified.
-
-For example, if a command has two parameter sets:
-	Name, ID  (default)
-	Name, Path
-This function returns:
-    Name (default), ID Path
-
-This function is used to get parameters for help and for help testing.
-
-.PARAMETER Command
-Enter a CommandInfo object, such as the object that Get-Command returns. You
-can also pipe a CommandInfo object to the function.
-
-This parameter takes a CommandInfo object, instead of a command name, so
-you can use the parameters of Get-Command to specify the module and version
-of the command.
-
-.EXAMPLE
-PS C:\> Get-ParametersDefaultFirst -Command (Get-Command New-Guid)
-This command uses the Command parameter to specify the command to
-Get-ParametersDefaultFirst
-
-.EXAMPLE
-PS C:\> Get-Command New-Guid | Get-ParametersDefaultFirst
-You can also pipe a CommandInfo object to Get-ParametersDefaultFirst
-
-.EXAMPLE
-PS C:\> Get-ParametersDefaultFirst -Command (Get-Command BetterCredentials\Get-Credential)
-You can use the Command parameter to specify the CommandInfo object. This
-command runs Get-Command module-qualified name value.
-
-.EXAMPLE
-PS C:\> $ModuleSpec = @{ModuleName='BetterCredentials';RequiredVersion=4.3}
-PS C:\> Get-Command -FullyQualifiedName $ModuleSpec | Get-ParametersDefaultFirst
-This command uses a Microsoft.PowerShell.Commands.ModuleSpecification object to
-specify the module and version. You can also use it to specify the module GUID.
-Then, it pipes the CommandInfo object to Get-ParametersDefaultFirst.
-#>
-function Get-ParametersDefaultFirst {
-	Param
-	(
-		[Parameter(Mandatory = $true,
-				   ValueFromPipeline = $true)]
-		[System.Management.Automation.CommandInfo]
-		$Command
-	)
-
-	BEGIN {
-		$Common = 'Debug', 'ErrorAction', 'ErrorVariable', 'InformationAction', 'InformationVariable', 'OutBuffer', 'OutVariable', 'PipelineVariable', 'Verbose', 'WarningAction', 'WarningVariable', 'WhatIf', 'Confirm'
-		$parameters = @()
-	}
-	PROCESS {
-		if ($defaultPSetName = $Command.DefaultParameterSet) {
-			$defaultParameters = ($Command.ParameterSets | Where-Object Name -eq $defaultPSetName).parameters | Where-Object Name -NotIn $common
-			$otherParameters = ($Command.ParameterSets | Where-Object Name -ne $defaultPSetName).parameters | Where-Object Name -NotIn $common
-
-			$parameters = $defaultParameters
-			if ($parameters -and $otherParameters) {
-				$otherParameters | ForEach-Object {
-					if ($_.Name -notin $parameters.Name) {
-						$parameters += $_
-					}
-				}
-				$parameters = $parameters | Sort-Object Name
-			}
-		}
-		else {
-			$parameters = $Command.ParameterSets.Parameters | Where-Object Name -NotIn $common | Sort-Object Name -Unique
-		}
-
-
-		return $parameters
-	}
-	END { }
-}
 
 <#
 .SYNOPSIS
@@ -207,16 +109,11 @@ function Get-CommandVersion {
 
 
 if (!$RequiredVersion) {
-	$RequiredVersion = (Get-Module $ModuleName -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1).Version
+	$RequiredVersion = (Get-Command Invoke-ScriptAnalyzer).Module.Version
 }
 
-# Remove all versions of the module from the session. Pester can't handle multiple versions.
-Get-Module $ModuleName | Remove-Module
-
-# Import the required version
-Import-Module $ModuleName -RequiredVersion $RequiredVersion -ErrorAction Stop
 $ms = $null
-$commands =$null
+$commands = $null
 $paramBlackList = @(
 	'AttachAndDebug' # Reason: When building with DEGUG configuration, an additional parameter 'AttachAndDebug' will be added to Invoke-ScriptAnalyzer and Invoke-Formatter, but there is no Help for those, as they are not intended for production usage.
 )
@@ -233,91 +130,171 @@ else {
 ## When testing help, remember that help is cached at the beginning of each session.
 ## To test, restart session.
 
-foreach ($command in $commands) {
-	$commandName = $command.Name
+$testCases = $commands.ForEach{
+	@{
+		Command              = $_
+		CommandName          = $_.Name
+		CommandModuleVersion = Get-CommandVersion -CommandInfo $_
+		Help                 = & {
+			$Help = Get-Help "${ModuleName}\$($_.Name)" -ErrorAction SilentlyContinue
+			if ($Help.Synopsis -like '*`[`<CommonParameters`>`]*')
+			{
+				$Help = Get-Help $commandName -ErrorAction SilentlyContinue
+			}
+			$Help
+		}
+	}
+}
 
-	# Get the module name and version of the command. Used in the Describe name.
-	$commandModuleVersion = Get-CommandVersion -CommandInfo $command
 
-	# The module-qualified command fails on Microsoft.PowerShell.Archive cmdlets
-	$Help = Get-Help $ModuleName\$commandName -ErrorAction SilentlyContinue
-	if ($Help.Synopsis -like '*`[`<CommonParameters`>`]*') {
-		$Help = Get-Help $commandName -ErrorAction SilentlyContinue
+Describe "Cmdlet help" {
+
+	# If help is not found, synopsis in auto-generated help is the syntax diagram
+	It "should not be auto-generated" -TestCases $testCases {
+		# Replace pester BeLike with powershell -like as pester 3.3.9 does not support BeLike
+		$Help.Synopsis -like '*`[`<CommonParameters`>`]*' | Should -BeFalse
 	}
 
-	Describe "Test help for $commandName in $($commandModuleVersion.ModuleName) ($($commandModuleVersion.Version))" {
+	# Should be a description for every function
+	It "gets description for <CommandName>" -TestCases $testCases {
+		$Help.Description | Should -Not -BeNullOrEmpty
+	}
 
-		# If help is not found, synopsis in auto-generated help is the syntax diagram
-		It "should not be auto-generated" {
-			# Replace pester BeLike with powershell -like as pester 3.3.9 does not support BeLike
-			$Help.Synopsis -like '*`[`<CommonParameters`>`]*' | Should Be $false
-		}
+	# Should be at least one example
+	It "gets example code from <CommandName>" -TestCases $testCases {
+		($Help.Examples.Example | Select-Object -First 1).Code | Should -Not -BeNullOrEmpty
+	}
 
-		# Should be a description for every function
-		It "gets description for $commandName" {
-			$Help.Description | Should -Not -BeNullOrEmpty
-		}
+	# Should be at least one example description
+	It "gets example help from $commandName" -TestCases $testCases {
+		($Help.Examples.Example.Remarks | Select-Object -First 1).Text | Should -Not -BeNullOrEmpty
+	}
+}
 
-		# Should be at least one example
-		It "gets example code from $commandName" {
-			($Help.Examples.Example | Select-Object -First 1).Code | Should -Not -BeNullOrEmpty
-		}
+Describe 'Cmdlet parameter help' {
+	BeforeAll {
+				<#
+		.SYNOPSIS
+		Gets command parameters; one per name. Prefers default parameter set.
 
-		# Should be at least one example description
-		It "gets example help from $commandName" {
-			($Help.Examples.Example.Remarks | Select-Object -First 1).Text | Should -Not -BeNullOrEmpty
-		}
+		.DESCRIPTION
+		Gets one CommandParameterInfo object for each parameter in the specified
+		command. If a command has more than one parameter with the same name, this
+		function gets the parameters in the default parameter set, if one is specified.
 
-		Context "Test parameter help for $commandName" {
+		For example, if a command has two parameter sets:
+			Name, ID  (default)
+			Name, Path
+		This function returns:
+			Name (default), ID Path
 
-			$Common = 'Debug', 'ErrorAction', 'ErrorVariable', 'InformationAction', 'InformationVariable', 'OutBuffer', 'OutVariable',
-			'PipelineVariable', 'Verbose', 'WarningAction', 'WarningVariable', 'WhatIf', 'Confirm'
+		This function is used to get parameters for help and for help testing.
 
-			# Get parameters. When >1 parameter with same name,
-			# get parameter from the default parameter set, if any.
-			$parameters = Get-ParametersDefaultFirst -Command $command
+		.PARAMETER Command
+		Enter a CommandInfo object, such as the object that Get-Command returns. You
+		can also pipe a CommandInfo object to the function.
 
-			$parameterNames = $parameters.Name
-			$HelpParameterNames = $Help.Parameters.Parameter.Name | Sort-Object -Unique
+		This parameter takes a CommandInfo object, instead of a command name, so
+		you can use the parameters of Get-Command to specify the module and version
+		of the command.
 
-			foreach ($parameter in $parameters) {
-				if ($parameter.Name -in $paramBlackList) {
-					continue
-				}
-				$parameterName = $parameter.Name
-				$parameterHelp = $Help.parameters.parameter | Where-Object Name -EQ $parameterName
+		.EXAMPLE
+		PS C:\> Get-ParametersDefaultFirst -Command (Get-Command New-Guid)
+		This command uses the Command parameter to specify the command to
+		Get-ParametersDefaultFirst
 
-				# Should be a description for every parameter
-				It "gets help for parameter: $parameterName : in $commandName" {
-					# `$parameterHelp.Description.Text | Should -Not -BeNullOrEmpty` fails for -Settings paramter
-					# without explicit [string] casting on the Text property
-					$parameterHelp.Description.Text | Should -Not -BeNullOrEmpty
-				}
+		.EXAMPLE
+		PS C:\> Get-Command New-Guid | Get-ParametersDefaultFirst
+		You can also pipe a CommandInfo object to Get-ParametersDefaultFirst
 
-				# Required value in Help should match IsMandatory property of parameter
-				It "help for $parameterName parameter in $commandName has correct Mandatory value" {
-					$codeMandatory = $parameter.IsMandatory.toString()
-					$parameterHelp.Required | Should -Be $codeMandatory
-				}
+		.EXAMPLE
+		PS C:\> Get-ParametersDefaultFirst -Command (Get-Command BetterCredentials\Get-Credential)
+		You can use the Command parameter to specify the CommandInfo object. This
+		command runs Get-Command module-qualified name value.
 
-				# Parameter type in Help should match code
-				It "help for $commandName has correct parameter type for $parameterName" {
-					$codeType = $parameter.ParameterType.Name
-					# To avoid calling Trim method on a null object.
-					$helpType = if ($parameterHelp.parameterValue) { $parameterHelp.parameterValue.Trim() }
-					$helpType | Should -Be $codeType
-				}
+		.EXAMPLE
+		PS C:\> $ModuleSpec = @{ModuleName='BetterCredentials';RequiredVersion=4.3}
+		PS C:\> Get-Command -FullyQualifiedName $ModuleSpec | Get-ParametersDefaultFirst
+		This command uses a Microsoft.PowerShell.Commands.ModuleSpecification object to
+		specify the module and version. You can also use it to specify the module GUID.
+		Then, it pipes the CommandInfo object to Get-ParametersDefaultFirst.
+		#>
+		function Get-ParametersDefaultFirst {
+			Param
+			(
+				[Parameter(Mandatory = $true,
+						ValueFromPipeline = $true)]
+				[System.Management.Automation.CommandInfo]
+				$Command
+			)
+
+			BEGIN {
+				$Common = 'Debug', 'ErrorAction', 'ErrorVariable', 'InformationAction', 'InformationVariable', 'OutBuffer', 'OutVariable', 'PipelineVariable', 'Verbose', 'WarningAction', 'WarningVariable', 'WhatIf', 'Confirm'
+				$parameters = @()
 			}
+			PROCESS {
+				if ($defaultPSetName = $Command.DefaultParameterSet) {
+					$defaultParameters = ($Command.ParameterSets | Where-Object Name -eq $defaultPSetName).parameters | Where-Object Name -NotIn $common
+					$otherParameters = ($Command.ParameterSets | Where-Object Name -ne $defaultPSetName).parameters | Where-Object Name -NotIn $common
 
-			foreach ($helpParm in $HelpParameterNames) {
-				if ($helpParm -in $paramBlackList) {
-					continue
+					$parameters = $defaultParameters
+					if ($parameters -and $otherParameters) {
+						$otherParameters | ForEach-Object {
+							if ($_.Name -notin $parameters.Name) {
+								$parameters += $_
+							}
+						}
+						$parameters = $parameters | Sort-Object Name
+					}
 				}
-				# Shouldn't find extra parameters in help.
-				It "finds help parameter in code: $helpParm" {
-					$helpParm -in $parameterNames | Should -BeTrue
+				else {
+					$parameters = $Command.ParameterSets.Parameters | Where-Object Name -NotIn $common | Sort-Object Name -Unique
 				}
+
+
+				return $parameters
 			}
+			END { }
+		}
+	}
+	It "Parameter help for <CommandName>" -TestCases $testCases {
+		# Get parameters. When >1 parameter with same name,
+		# get parameter from the default parameter set, if any.
+		$parameters = Get-ParametersDefaultFirst -Command $command
+
+		$parameterNames = $parameters.Name
+		$HelpParameterNames = $Help.Parameters.Parameter.Name | Sort-Object -Unique
+
+		foreach ($parameter in $parameters) {
+			if ($parameter.Name -in $paramBlackList) {
+				continue
+			}
+			$parameterName = $parameter.Name
+			$parameterHelp = $Help.parameters.parameter | Where-Object Name -EQ $parameterName
+
+			# Should be a description for every parameter
+			# It "gets help for parameter: $parameterName : in $commandName" {
+				# `$parameterHelp.Description.Text | Should -Not -BeNullOrEmpty` fails for -Settings paramter
+				# without explicit [string] casting on the Text property
+				$parameterHelp.Description.Text | Should -Not -BeNullOrEmpty -Because "Parameter '$parameterName' of command '$CommandName' should have a Description"
+			# }
+
+			# Required value in Help should match IsMandatory property of parameter
+			$codeMandatory = $parameter.IsMandatory.toString()
+			$parameterHelp.Required | Should -Be $codeMandatory -Because "Parameter '$parameterName' of command '$CommandName should have the correct IsMandatory attribute"
+
+			# Parameter type in Help should match code
+			$codeType = $parameter.ParameterType.Name
+			# To avoid calling Trim method on a null object.
+			$helpType = if ($parameterHelp.parameterValue) { $parameterHelp.parameterValue.Trim() }
+			$helpType | Should -Be $codeType -Because "help for $commandName has correct parameter type for $parameterName"
+		}
+
+		foreach ($helpParm in $HelpParameterNames) {
+			if ($helpParm -in $paramBlackList) {
+				continue
+			}
+			$helpParm -in $parameterNames | Should -BeTrue -Because 'There should be no extra parameters in help'
 		}
 	}
 }
