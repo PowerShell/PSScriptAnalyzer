68312970564c35976968b7ffc3a50b2104b2adba
Christoph Bergmeister <christoph.bergmeister@bjss.com>
2020-04-15 21:09:18 +0100

    Properly execute library tests and cleanup CustomizedRule.tests.ps1

--- Tests/Engine/CustomizedRule.tests.ps1
+++ Tests/Engine/CustomizedRule.tests.ps1
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 BeforeAll {
-	if (-not ([bool] $IsCoreCLR))
+	if (-not $IsCoreCLR)
 	{
 		# Force Get-Help not to prompt for interactive input to download help using Update-Help
 		# By adding this registry key we turn off Get-Help interactivity logic during ScriptRule parsing
@@ -42,40 +42,40 @@ Describe "Test importing customized rules with null return results" {
 
 Describe "Test importing correct customized rules" {
 
-	if(-not ([bool] $IsCoreCLR))
-	{
-		Context "Test Get-Help functionality in ScriptRule parsing logic" {
-			It "ScriptRule help section must be correctly processed when Get-Help is called for the first time" {
+	# if(-not ([bool] $IsCoreCLR))
+	# {
+	Context "Test Get-Help functionality in ScriptRule parsing logic" -Skip:$IsCoreCLR {
+		It "ScriptRule help section must be correctly processed when Get-Help is called for the first time" {
 
-				# Force Get-Help to prompt for interactive input to download help using Update-Help
-				# By removing this registry key we force to turn on Get-Help interactivity logic during ScriptRule parsing
-				$null,"Wow6432Node" | ForEach-Object {
-					try
-					{
-						Remove-ItemProperty -Name "DisablePromptToUpdateHelp" -Path "HKLM:\SOFTWARE\$($_)\Microsoft\PowerShell" -ErrorAction Stop
-					} catch {
-						#Ignore for cases when tests are running in non-elevated more or registry key does not exist or not accessible
-					}
+			# Force Get-Help to prompt for interactive input to download help using Update-Help
+			# By removing this registry key we force to turn on Get-Help interactivity logic during ScriptRule parsing
+			$null,"Wow6432Node" | ForEach-Object {
+				try
+				{
+					Remove-ItemProperty -Name "DisablePromptToUpdateHelp" -Path "HKLM:\SOFTWARE\$($_)\Microsoft\PowerShell" -ErrorAction Stop
+				} catch {
+					#Ignore for cases when tests are running in non-elevated more or registry key does not exist or not accessible
 				}
+			}
 
-				$customizedRulePath = Invoke-ScriptAnalyzer $PSScriptRoot\TestScript.ps1 -CustomizedRulePath $PSScriptRoot\samplerule\samplerule.psm1 | Where-Object {$_.Message -eq $message}
-				$customizedRulePath.Count | Should -Be 1
+			$customizedRulePath = Invoke-ScriptAnalyzer $PSScriptRoot\TestScript.ps1 -CustomizedRulePath $PSScriptRoot\samplerule\samplerule.psm1 | Where-Object {$_.Message -eq $message}
+			$customizedRulePath.Count | Should -Be 1
 
-				# Force Get-Help not to prompt for interactive input to download help using Update-Help
-				# By adding this registry key we turn off Get-Help interactivity logic during ScriptRule parsing
-				$null,"Wow6432Node" | ForEach-Object {
-					try
-					{
-						Set-ItemProperty -Name "DisablePromptToUpdateHelp" -Path "HKLM:\SOFTWARE\$($_)\Microsoft\PowerShell" -Value 1 -Force -EA SilentlyContinue
-					}
-					catch
-					{
-						# Ignore for cases when tests are running in non-elevated more or registry key does not exist or not accessible
-					}
+			# Force Get-Help not to prompt for interactive input to download help using Update-Help
+			# By adding this registry key we turn off Get-Help interactivity logic during ScriptRule parsing
+			$null,"Wow6432Node" | ForEach-Object {
+				try
+				{
+					Set-ItemProperty -Name "DisablePromptToUpdateHelp" -Path "HKLM:\SOFTWARE\$($_)\Microsoft\PowerShell" -Value 1 -Force -EA SilentlyContinue
+				}
+				catch
+				{
+					# Ignore for cases when tests are running in non-elevated more or registry key does not exist or not accessible
 				}
 			}
 		}
 	}
+	# }
 
     Context "Test Get-ScriptAnalyzer with customized rules" {
         It "will show the custom rule" {
@@ -201,8 +201,9 @@ Describe "Test importing correct customized rules" {
             $violations[0].RuleSuppressionID   | Should -Be "MyRuleSuppressionID"
         }
 
-        if (!$testingLibraryUsage)
-		{
+        # if (!$testingLibraryUsage)
+		# {
+		Context "Test Invoke-ScriptAnalyzer with customized rules - Advanced test cases" -Skip:$testingLibraryUsage {
             It "will show the custom rule in the results when given a rule folder path with trailing backslash" {
 				# needs fixing for Linux
 				if (!$IsLinux -and !$IsMacOS)
