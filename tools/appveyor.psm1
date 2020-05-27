8baab131fcbba973c196fdbbea1304d033baefaa
Christoph Bergmeister <christoph.bergmeister@bjss.com>
2020-05-27 17:49:41 +0100

    pester 5 GA

--- tools/appveyor.psm1
+++ tools/appveyor.psm1
@@ -10,12 +10,12 @@ function Install-Pester {
         if ($null -eq (Get-Module -ListAvailable PowershellGet)) {
             # WMF 4 image build
             Write-Verbose -Verbose "Installing Pester via nuget"
-            nuget install Pester -Version "${requiredPesterVersion}-rc9" -source https://www.powershellgallery.com/api/v2 -outputDirectory "$env:ProgramFiles\WindowsPowerShell\Modules\." -ExcludeVersion
+            nuget install Pester -Version $requiredPesterVersion -source https://www.powershellgallery.com/api/v2 -outputDirectory "$env:ProgramFiles\WindowsPowerShell\Modules\." -ExcludeVersion
         }
         else {
             # Visual Studio 2017 build (has already Pester v3, therefore a different installation mechanism is needed to make it also use the new version 4)
             Write-Verbose -Verbose "Installing Pester via Install-Module"
-            Install-Module -Name Pester -Force -SkipPublisherCheck -Scope CurrentUser -Repository PSGallery -AllowPrerelease
+            Install-Module -Name Pester -Force -SkipPublisherCheck -Scope CurrentUser -Repository PSGallery
         }
     }
 }
