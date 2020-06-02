b4eb8718224e2c968208b17afc707939f5ba03d0
Christoph Bergmeister [MVP] <c.bergmeister@gmail.com>
2020-06-02 18:25:48 +0100

    Pester 5.0.2

--- tools/appveyor.psm1
+++ tools/appveyor.psm1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = 'Stop'
 
 function Install-Pester {
-    $requiredPesterVersion = '5.0.0'
+    $requiredPesterVersion = '5.0.2'
     $pester = Get-Module Pester -ListAvailable | Where-Object { $_.Version -eq $requiredPesterVersion }
     if ($null -eq $pester) {
         if ($null -eq (Get-Module -ListAvailable PowershellGet)) {
