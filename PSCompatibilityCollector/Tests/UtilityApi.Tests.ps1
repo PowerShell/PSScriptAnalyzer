932ca1033b905513d775215c6f8880adecd2ff79
Christoph Bergmeister <christoph.bergmeister@bjss.com>
2020-06-07 20:56:30 +0100

    move testcases declaration before test

--- PSCompatibilityCollector/Tests/UtilityApi.Tests.ps1
+++ PSCompatibilityCollector/Tests/UtilityApi.Tests.ps1
@@ -23,10 +23,7 @@ BeforeAll {
 
 Describe "Type name serialization" {
 
-    It "Serializes the name of type <InputType> to <ExpectedName>" {
-        $name = [Microsoft.PowerShell.CrossCompatibility.TypeNaming]::GetFullTypeName($InputType)
-        $name | Should -BeExactly $ExpectedName
-    } -TestCases @(
+    It "Serializes the name of type <InputType> to <ExpectedName>" -TestCases @(
         @{ InputType = [System.Reflection.Assembly]; ExpectedName = "System.Reflection.Assembly" }
         @{ InputType = [string]; ExpectedName = "System.String" }
         @{ InputType = [datetime]; ExpectedName = "System.DateTime" }
@@ -35,12 +32,15 @@ Describe "Type name serialization" {
         @{ InputType = [System.Func`1]; ExpectedName = "System.Func``1" }
         @{ InputType = [System.Collections.Generic.Dictionary`2]; ExpectedName = "System.Collections.Generic.Dictionary``2" }
         @{ InputType = [System.Collections.Generic.Dictionary`2+Enumerator]; ExpectedName = "System.Collections.Generic.Dictionary``2+Enumerator" }
-        @{ InputType = [System.Collections.Generic.Dictionary[string,object]].GetNestedType('Enumerator'); ExpectedName = "System.Collections.Generic.Dictionary``2+Enumerator" }
+        @{ InputType = [System.Collections.Generic.Dictionary[string, object]].GetNestedType('Enumerator'); ExpectedName = "System.Collections.Generic.Dictionary``2+Enumerator" }
         @{ InputType = [System.Collections.Generic.List[object]]; ExpectedName = "System.Collections.Generic.List``1[System.Object]" }
         @{ InputType = [System.Collections.Generic.Dictionary[string, object]]; ExpectedName = "System.Collections.Generic.Dictionary``2[System.String,System.Object]" }
-        @{ InputType = [System.Collections.Generic.Dictionary`2+Enumerator[string,object]]; ExpectedName = "System.Collections.Generic.Dictionary``2+Enumerator[System.String,System.Object]" }
-        @{ InputType = [System.Collections.Concurrent.ConcurrentDictionary`2].GetMethod('ToArray').ReturnType; ExpectedName = "System.Collections.Generic.KeyValuePair``2[]"}
-    )
+        @{ InputType = [System.Collections.Generic.Dictionary`2+Enumerator[string, object]]; ExpectedName = "System.Collections.Generic.Dictionary``2+Enumerator[System.String,System.Object]" }
+        @{ InputType = [System.Collections.Concurrent.ConcurrentDictionary`2].GetMethod('ToArray').ReturnType; ExpectedName = "System.Collections.Generic.KeyValuePair``2[]" }
+    ) {
+        $name = [Microsoft.PowerShell.CrossCompatibility.TypeNaming]::GetFullTypeName($InputType)
+        $name | Should -BeExactly $ExpectedName
+    }
 
     It "Null type throws exception" {
         {
@@ -67,14 +67,7 @@ Describe "Type accelerator expansion" {
             ForEach-Object { $d = New-Object 'System.Collections.Generic.Dictionary[string,string]' } { $d.Add($_.Key, $_.Value.FullName) } { $d }
     }
 
-    It "Expands the typename in <Raw> to <Expanded>" {
-
-        $typeName = Get-TypeNameAstFromScript -Script $Raw
-
-        $canonicalName = [Microsoft.PowerShell.CrossCompatibility.TypeNaming]::GetCanonicalTypeName($typeAccelerators, $typeName)
-
-        $canonicalName | Should -BeExactly $Expanded
-    } -TestCases @(
+    It "Expands the typename in <Raw> to <Expanded>" -TestCases @(
         @{ Raw = "[System.Exception]"; Expanded = "System.Exception" }
         @{ Raw = "[string]"; Expanded = "System.String" }
         @{ Raw = "[psmoduleinfo]"; Expanded = "System.Management.Automation.PSModuleInfo" }
@@ -86,7 +79,14 @@ Describe "Type accelerator expansion" {
         @{ Raw = "[System.Collections.Generic.Dictionary``2[string,psmoduleinfo]]"; Expanded = "System.Collections.Generic.Dictionary``2[System.String,System.Management.Automation.PSModuleInfo]" }
         @{ Raw = "[System.Collections.Generic.Dictionary``2  [string, psmoduleinfo]]"; Expanded = "System.Collections.Generic.Dictionary``2[System.String,System.Management.Automation.PSModuleInfo]" }
         @{ Raw = "[object]"; Expanded = "System.Object" }
-    )
+    ) -Test {
+
+        $typeName = Get-TypeNameAstFromScript -Script $Raw
+
+        $canonicalName = [Microsoft.PowerShell.CrossCompatibility.TypeNaming]::GetCanonicalTypeName($typeAccelerators, $typeName)
+
+        $canonicalName | Should -BeExactly $Expanded
+    }
 }
 
 $versionCreationTests = @(
@@ -114,22 +114,29 @@ if ($PSVersionTable.PSVersion.Major -ge 6)
 Describe "PowerShell version object" {
     Context "Version parsing" {
 
-        It "Parses version string '<VerStr>' as <Major>.<Minor>.<Patch>" {
+        It "Parses version string '<VerStr>' as <Major>.<Minor>.<Patch>" -TestCases @(
+            @{ VerStr = '42'; Major = 42; Minor = -1; Patch = -1 }
+            @{ VerStr = '7'; Major = 7; Minor = -1; Patch = -1 }
+            @{ VerStr = '6.1'; Major = 6; Minor = 1; Patch = -1 }
+            @{ VerStr = '5.2.7'; Major = 5; Minor = 2; Patch = 7 }
+            @{ VerStr = '512.2124.71'; Major = 512; Minor = 2124; Patch = 71 }
+        ) -Test {
 
             $v = [Microsoft.PowerShell.CrossCompatibility.PowerShellVersion]$VerStr
 
             $v.Major | Should -Be $Major
             $v.Minor | Should -Be $Minor
             $v.Patch | Should -Be $Patch
-        } -TestCases @(
-            @{ VerStr = '42'; Major = 42; Minor = -1; Patch = -1 }
-            @{ VerStr = '7'; Major = 7; Minor = -1; Patch = -1 }
-            @{ VerStr = '6.1'; Major = 6; Minor = 1; Patch = -1 }
-            @{ VerStr = '5.2.7'; Major = 5; Minor = 2; Patch = 7 }
-            @{ VerStr = '512.2124.71'; Major = 512; Minor = 2124; Patch = 71 }
-        )
+        }
 
-        It "Parses version string '<VerStr>' as <Major>.<Minor>.<Patch>-<Label>+<BuildLabel>" {
+        It "Parses version string '<VerStr>' as <Major>.<Minor>.<Patch>-<Label>+<BuildLabel>" -TestCases @(
+            @{ VerStr = '6.1.0-rc.1'; Major = 6; Minor = 1; Patch = 0; Label = 'rc.1' }
+            @{ VerStr = '6.2-preview.2'; Major = 6; Minor = 2; Patch = -1; Label = 'preview.2' }
+            @{ VerStr = '6-preview.2'; Major = 6; Minor = -1; Patch = -1; Label = 'preview.2' }
+            @{ VerStr = '6.1.0-rc.1+moo'; Major = 6; Minor = 1; Patch = 0; Label = 'rc.1'; BuildLabel = 'moo' }
+            @{ VerStr = '6.2-preview.2+horse'; Major = 6; Minor = 2; Patch = -1; Label = 'preview.2'; BuildLabel = 'horse' }
+            @{ VerStr = '6-preview.2+veryimportant'; Major = 6; Minor = -1; Patch = -1; Label = 'preview.2'; BuildLabel = 'veryimportant' }
+        ) -Test {
             param([string]$VerStr, [int]$Major, [int]$Minor, [int]$Patch, [string]$Label, $BuildLabel)
 
             $v = [Microsoft.PowerShell.CrossCompatibility.PowerShellVersion]$VerStr
@@ -139,16 +146,11 @@ Describe "PowerShell version object" {
             $v.Patch | Should -Be $Patch
             $v.PreReleaseLabel | Should -BeExactly $Label
             $v.BuildLabel | Should -Be $BuildLabel
-        } -TestCases @(
-            @{ VerStr = '6.1.0-rc.1'; Major = 6; Minor = 1; Patch = 0; Label = 'rc.1' }
-            @{ VerStr = '6.2-preview.2'; Major = 6; Minor = 2; Patch = -1; Label = 'preview.2' }
-            @{ VerStr = '6-preview.2'; Major = 6; Minor = -1; Patch = -1; Label = 'preview.2' }
-            @{ VerStr = '6.1.0-rc.1+moo'; Major = 6; Minor = 1; Patch = 0; Label = 'rc.1'; BuildLabel = 'moo' }
-            @{ VerStr = '6.2-preview.2+horse'; Major = 6; Minor = 2; Patch = -1; Label = 'preview.2'; BuildLabel = 'horse' }
-            @{ VerStr = '6-preview.2+veryimportant'; Major = 6; Minor = -1; Patch = -1; Label = 'preview.2'; BuildLabel = 'veryimportant' }
-        )
+        }
 
-        It "Parses version string '<VerStr>' as <Major>.<Minor>.<Patch>.<Revision>" {
+        It "Parses version string '<VerStr>' as <Major>.<Minor>.<Patch>.<Revision>"-TestCases @(
+            @{ VerStr = '5.2.1.12312'; Major = 5; Minor = 2; Patch = 1; Revision = 12312 }
+        ) -Test {
             param([string]$VerStr, [int]$Major, [int]$Minor, [int]$Patch, [int]$Revision)
 
             $v = [Microsoft.PowerShell.CrossCompatibility.PowerShellVersion]$VerStr
@@ -157,15 +159,9 @@ Describe "PowerShell version object" {
             $v.Minor | Should -Be $Minor
             $v.Patch | Should -Be $Patch
             $v.Revision | Should -Be $Revision
-        } -TestCases @(
-            @{ VerStr = '5.2.1.12312'; Major = 5; Minor = 2; Patch = 1; Revision = 12312 }
-        )
+        }
 
-        It "Does not parse '<VerStr>' as a version" {
-            param([string]$VerStr)
-
-            { [Microsoft.PowerShell.CrossCompatibility.PowerShellVersion]$VerStr } | Should -Throw
-        } -TestCases @(
+        It "Does not parse '<VerStr>' as a version" -TestCases @(
             @{ VerStr = 'banana' }
             @{ VerStr = '' }
             @{ VerStr = '1.' }
@@ -176,7 +172,11 @@ Describe "PowerShell version object" {
             @{ VerStr = '4.1.5.7.4' }
             @{ VerStr = '4.1.5.7-rc.2' }
             @{ VerStr = '4.1.5.-rc.2' }
-        )
+        ) -Test {
+            param([string]$VerStr)
+
+            { [Microsoft.PowerShell.CrossCompatibility.PowerShellVersion]$VerStr } | Should -Throw
+        }
     }
 
     Context "Version creation from other versions" {
