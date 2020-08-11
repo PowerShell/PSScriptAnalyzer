7ef643b78d858cfb0eadec12d831556f2ec163d0
Joel Davies <12704625+daviesj@users.noreply.github.com>
2020-08-11 18:51:44 -0500

    Fleshed out FindAstPositionVistor and fixed test

--- Tests/Engine/TokenOperations.tests.ps1
+++ Tests/Engine/TokenOperations.tests.ps1
@@ -13,7 +13,7 @@ $h = @{
         $parseErrors = $null
         $scriptAst = [System.Management.Automation.Language.Parser]::ParseInput($scriptText, [ref] $tokens, [ref] $parseErrors)
         $tokenOperations = New-Object Microsoft.Windows.PowerShell.ScriptAnalyzer.TokenOperations -ArgumentList @($tokens, $scriptAst)
-        $operatorToken = $tokens | Where-Object { $_.Extent.StartOffset -eq 32 }
+        $operatorToken = $tokens | Where-Object { $_.Extent.StartLineNumber -eq 3 -and $_.Extent.StartColumnNumber -eq 14}
         $hashTableAst = $tokenOperations.GetAstPosition($operatorToken)
         $hashTableAst | Should -BeOfType [System.Management.Automation.Language.HashTableAst]
         $hashTableAst.Extent.Text | Should -Be '@{ z = "hi" }'
