f71cad7719607be77d7c498a07b7314fc753fa8d
James Truher [MSFT] <jimtru@microsoft.com>
2020-08-18 11:52:20 -0700

    Fix missing space in AvoidPositionalParameter test asset (#1573)

--- Tests/Rules/AvoidPositionalParametersNoViolations.ps1
+++ Tests/Rules/AvoidPositionalParametersNoViolations.ps1
@@ -3,7 +3,7 @@ Get-ChildItem -Path "Tests"
 Clear-Host
 Split-Path -Path "Random" -leaf
 Get-Process | Where-Object {$_.handles -gt 200}
-get-service-computername localhost | where {($_.status -eq "Running") -and ($_.CanStop -eq $true)}
+get-service -computername localhost | where {($_.status -eq "Running") -and ($_.CanStop -eq $true)}
 1, 2, $null, 4 | ForEach-Object {"Hello"}
 & "$env:Windir\System32\Calc.exe" Parameter1 Parameter2
 
