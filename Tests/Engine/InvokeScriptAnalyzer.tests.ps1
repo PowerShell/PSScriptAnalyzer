ea28d2b3cc582e6227c2395c3d8927bc0070bce3
Robert Holt <rjmholt_msft@outlook.com>
2021-07-24 15:09:18 -0700

    Show command

--- Tests/Engine/InvokeScriptAnalyzer.tests.ps1
+++ Tests/Engine/InvokeScriptAnalyzer.tests.ps1
@@ -10,6 +10,7 @@ BeforeAll {
     $useRules = "PSUse*"
 
     # Dump out Invoke-ScriptAnalyzer info to debug weird failures
+    Write-Verbose -Verbose "Command: $($sa)"
     Write-Verbose -Verbose "Module: $($sa.Module.Path)"
     Write-Verbose -Verbose "Version: $($sa.Module.Version)"
 }
