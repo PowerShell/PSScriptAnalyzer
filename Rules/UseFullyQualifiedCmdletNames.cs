69a2690518f6df3ce4309791e0ee1a8d1e90daf3
René Vaessen <rene.vaessen@gmail.com>
2025-08-19 06:25:37 +0200

    Added UseFullyQualifiedCmdletNames rule

--- Rules/UseFullyQualifiedCmdletNames.cs
+++ Rules/UseFullyQualifiedCmdletNames.cs
@@ -23,6 +23,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Management.Automation;
