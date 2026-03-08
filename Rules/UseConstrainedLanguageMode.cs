e616116ed39b5717611485f6da5661f2374effd4
Josh Corrick <joshcorr@users.noreply.github.com>
2026-03-07 16:02:59 -0800

    Enhance CLM rule: restrict types, COM objects, add tests

--- Rules/UseConstrainedLanguageMode.cs
+++ Rules/UseConstrainedLanguageMode.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation.Language;
 using Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic;
+using System.Management.Automation;
+
 #if !CORECLR
 using System.ComponentModel.Composition;
 #endif
@@ -21,6 +23,80 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
 #endif
     public class UseConstrainedLanguageMode : ConfigurableRule
     {
+        // Allowed COM objects in Constrained Language Mode
+        private static readonly HashSet<string> AllowedComObjects = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "Scripting.Dictionary",
+            "Scripting.FileSystemObject",
+            "VBScript.RegExp"
+        };
+
+        // Allowed types in Constrained Language Mode (type accelerators and common types)
+        private static readonly HashSet<string> AllowedTypes = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "adsi", "adsisearcher", "Alias", "AllowEmptyCollection", "AllowEmptyString",
+            "AllowNull", "ArgumentCompleter", "ArgumentCompletions", "array", "bigint",
+            "bool", "byte", "char", "cimclass", "cimconverter", "ciminstance", "CimSession",
+            "cimtype", "CmdletBinding", "cultureinfo", "datetime", "decimal", "double",
+            "DscLocalConfigurationManager", "DscProperty", "DscResource", "ExperimentAction",
+            "Experimental", "ExperimentalFeature", "float", "guid", "hashtable", "int",
+            "int16", "int32", "int64", "ipaddress", "IPEndpoint", "long", "mailaddress",
+            "Microsoft.PowerShell.Commands.ModuleSpecification", "NoRunspaceAffinity",
+            "NullString", "Object[]", "ObjectSecurity", "ordered", "OutputType", "Parameter",
+            "PhysicalAddress", "pscredential", "pscustomobject", "PSDefaultValue",
+            "pslistmodifier", "psobject", "psprimitivedictionary", "PSTypeNameAttribute",
+            "regex", "sbyte", "securestring", "semver", "short", "single", "string",
+            "SupportsWildcards", "switch", "timespan", "uint", "uint16", "uint32", "uint64",
+            "ulong", "uri", "ushort", "ValidateCount", "ValidateDrive", "ValidateLength",
+            "ValidateNotNull", "ValidateNotNullOrEmpty", "ValidateNotNullOrWhiteSpace",
+            "ValidatePattern", "ValidateRange", "ValidateScript", "ValidateSet",
+            "ValidateTrustedData", "ValidateUserDrive", "version", "void", "WildcardPattern",
+            "wmi", "wmiclass", "wmisearcher", "X500DistinguishedName", "X509Certificate", "xml",
+            // Full type names for common allowed types
+            "System.Object", "System.String", "System.Int32", "System.Boolean", "System.Byte",
+            "System.Collections.Hashtable", "System.DateTime", "System.Version", "System.Uri",
+            "System.Guid", "System.TimeSpan", "System.Management.Automation.PSCredential",
+            "System.Management.Automation.PSObject", "System.Security.SecureString",
+            "System.Text.RegularExpressions.Regex", "System.Xml.XmlDocument",
+            "System.Collections.ArrayList", "System.Collections.Generic.List",
+            "System.Net.IPAddress", "System.Net.Mail.MailAddress"
+        };
+
+        public UseConstrainedLanguageMode()
+        {
+            // This rule is disabled by default - users must explicitly enable it
+            Enable = false;
+        }
+
+        /// <summary>
+        /// Checks if a type name is allowed in Constrained Language Mode
+        /// </summary>
+        private bool IsTypeAllowed(string typeName)
+        {
+            if (string.IsNullOrWhiteSpace(typeName))
+            {
+                return true; // Can't determine, so don't flag
+            }
+
+            // Check exact match first
+            if (AllowedTypes.Contains(typeName))
+            {
+                return true;
+            }
+
+            // Check simple name (last part after last dot)
+            if (typeName.Contains('.'))
+            {
+                var simpleTypeName = typeName.Substring(typeName.LastIndexOf('.') + 1);
+                if (AllowedTypes.Contains(simpleTypeName))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         /// <summary>
         /// Analyzes the script to check for patterns that may require Constrained Language Mode.
         /// </summary>
@@ -52,7 +128,7 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
                     ));
             }
 
-            // Check for New-Object with COM objects (not allowed in Constrained Language Mode)
+            // Check for New-Object with COM objects and TypeName (only specific ones are allowed in CLM)
             var newObjectCommands = ast.FindAll(testAst =>
                 testAst is CommandAst cmdAst &&
                 cmdAst.GetCommandName() != null &&
@@ -61,20 +137,66 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
 
             foreach (CommandAst cmd in newObjectCommands)
             {
-                // Check if -ComObject parameter is used
-                var comObjectParam = cmd.CommandElements.OfType<CommandParameterAst>()
-                    .FirstOrDefault(p => p.ParameterName.Equals("ComObject", StringComparison.OrdinalIgnoreCase));
-
-                if (comObjectParam != null)
+                // Use StaticParameterBinder to reliably get parameter values
+                var bindingResult = StaticParameterBinder.BindCommand(cmd, true);
+                
+                // Check for -ComObject parameter
+                if (bindingResult.BoundParameters.ContainsKey("ComObject"))
                 {
-                    diagnosticRecords.Add(
-                        new DiagnosticRecord(
-                            String.Format(CultureInfo.CurrentCulture, Strings.UseConstrainedLanguageModeComObjectError),
-                            cmd.Extent,
-                            GetName(),
-                            GetDiagnosticSeverity(),
-                            fileName
-                        ));
+                    string comObjectValue = null;
+                    
+                    // Try to get the value from the AST directly first
+                    if (bindingResult.BoundParameters["ComObject"].Value is StringConstantExpressionAst strAst)
+                    {
+                        comObjectValue = strAst.Value;
+                    }
+                    else
+                    {
+                        // Fall back to ConstantValue
+                        comObjectValue = bindingResult.BoundParameters["ComObject"].ConstantValue as string;
+                    }
+                    
+                    // Only flag if COM object name was found AND it's not in the allowed list
+                    if (!string.IsNullOrWhiteSpace(comObjectValue) && !AllowedComObjects.Contains(comObjectValue))
+                    {
+                        diagnosticRecords.Add(
+                            new DiagnosticRecord(
+                                String.Format(CultureInfo.CurrentCulture, 
+                                    Strings.UseConstrainedLanguageModeComObjectError, 
+                                    comObjectValue),
+                                cmd.Extent,
+                                GetName(),
+                                GetDiagnosticSeverity(),
+                                fileName
+                            ));
+                    }
+                }
+                
+                // Check for -TypeName parameter
+                if (bindingResult.BoundParameters.ContainsKey("TypeName"))
+                {
+                    var typeNameValue = bindingResult.BoundParameters["TypeName"].ConstantValue as string;
+                    
+                    // If ConstantValue is null, try to extract from the AST Value
+                    if (typeNameValue == null && bindingResult.BoundParameters["TypeName"].Value is StringConstantExpressionAst typeStrAst)
+                    {
+                        typeNameValue = typeStrAst.Value;
+                    }
+                    
+                    // Only flag if type name was found AND it's not in the allowed list
+                    if (!string.IsNullOrWhiteSpace(typeNameValue) && !IsTypeAllowed(typeNameValue))
+                    {
+                        diagnosticRecords.Add(
+                            new DiagnosticRecord(
+                                String.Format(CultureInfo.CurrentCulture, 
+                                    Strings.UseConstrainedLanguageModeNewObjectError, 
+                                    typeNameValue),
+                                cmd.Extent,
+                                GetName(),
+                                GetDiagnosticSeverity(),
+                                fileName
+                            ));
+                    }
                 }
             }
 
@@ -144,6 +266,26 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
                         GetDiagnosticSeverity(),
                         fileName
                     ));
+            }
+
+            // Check for disallowed type accelerators and type constraints
+            var typeConstraints = ast.FindAll(testAst => testAst is TypeConstraintAst, true);
+            foreach (TypeConstraintAst typeConstraint in typeConstraints)
+            {
+                var typeName = typeConstraint.TypeName.FullName;
+                if (!IsTypeAllowed(typeName))
+                {
+                    diagnosticRecords.Add(
+                        new DiagnosticRecord(
+                            String.Format(CultureInfo.CurrentCulture,
+                                Strings.UseConstrainedLanguageModeConstrainedTypeError,
+                                typeName),
+                            typeConstraint.Extent,
+                            GetName(),
+                            GetDiagnosticSeverity(),
+                            fileName
+                        ));
+                }
             }
 
             return diagnosticRecords;
--- Tests/Rules/UseConstrainedLanguageMode.tests.ps1
+++ Tests/Rules/UseConstrainedLanguageMode.tests.ps1
@@ -5,7 +5,6 @@ BeforeAll {
     $testRootDirectory = Split-Path -Parent $PSScriptRoot
     Import-Module (Join-Path $testRootDirectory "PSScriptAnalyzerTestHelper.psm1")
 
-    $violationMessage = "is not permitted in Constrained Language Mode"
     $violationName = "PSUseConstrainedLanguageMode"
     $ruleName = $violationName
     
@@ -33,7 +32,7 @@ Add-Type -TypeDefinition @"
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
             $violations.Count | Should -Be 1
             $violations[0].RuleName | Should -Be $violationName
-            $violations[0].Message | Should -BeLike "*Add-Type*$violationMessage*"
+            $violations[0].Message | Should -BeLike "*Add-Type*"
         }
 
         It "Should not flag other commands" {
@@ -44,18 +43,44 @@ Add-Type -TypeDefinition @"
     }
 
     Context "When New-Object with COM is used" {
-        It "Should detect New-Object -ComObject usage" {
-            $def = 'New-Object -ComObject "Scripting.FileSystemObject"'
+        It "Should detect disallowed New-Object -ComObject usage" {
+            $def = 'New-Object -ComObject "Excel.Application"'
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
             $matchingViolations = $violations | Where-Object { $_.RuleName -eq $violationName }
             $matchingViolations.Count | Should -Be 1
-            $matchingViolations[0].Message | Should -BeLike "*COM object*$violationMessage*"
+            $matchingViolations[0].Message | Should -BeLike "*COM object*"
         }
 
-        It "Should not flag New-Object without -ComObject" {
+        It "Should NOT flag allowed COM objects - Scripting.Dictionary" {
+            $def = 'New-Object -ComObject "Scripting.Dictionary"'
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations | Where-Object { $_.RuleName -eq $violationName } | Should -BeNullOrEmpty
+        }
+
+        It "Should NOT flag allowed COM objects - Scripting.FileSystemObject" {
+            $def = 'New-Object -ComObject "Scripting.FileSystemObject"'
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations | Where-Object { $_.RuleName -eq $violationName } | Should -BeNullOrEmpty
+        }
+
+        It "Should NOT flag allowed COM objects - VBScript.RegExp" {
+            $def = 'New-Object -ComObject "VBScript.RegExp"'
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations | Where-Object { $_.RuleName -eq $violationName } | Should -BeNullOrEmpty
+        }
+
+        It "Should NOT flag New-Object with allowed TypeName" {
             $def = 'New-Object -TypeName System.Collections.ArrayList'
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
             $violations | Where-Object { $_.RuleName -eq $violationName } | Should -BeNullOrEmpty
+        }
+
+        It "Should flag New-Object with disallowed TypeName" {
+            $def = 'New-Object -TypeName System.Net.WebClient'
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $matchingViolations = $violations | Where-Object { $_.RuleName -eq $violationName }
+            $matchingViolations.Count | Should -Be 1
+            $matchingViolations[0].Message | Should -BeLike "*System.Net.WebClient*not permitted*"
         }
     }
 
@@ -71,7 +96,7 @@ $xaml = @"
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
             $matchingViolations = $violations | Where-Object { $_.RuleName -eq $violationName }
             $matchingViolations.Count | Should -Be 1
-            $matchingViolations[0].Message | Should -BeLike "*XAML*$violationMessage*"
+            $matchingViolations[0].Message | Should -BeLike "*XAML*"
         }
     }
 
@@ -81,7 +106,7 @@ $xaml = @"
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
             $matchingViolations = $violations | Where-Object { $_.RuleName -eq $violationName }
             $matchingViolations.Count | Should -Be 1
-            $matchingViolations[0].Message | Should -BeLike "*Invoke-Expression*$violationMessage*"
+            $matchingViolations[0].Message | Should -BeLike "*Invoke-Expression*"
         }
     }
 
@@ -91,6 +116,22 @@ $xaml = @"
             $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
             $matchingViolations = $violations | Where-Object { $_.RuleName -eq $violationName }
             $matchingViolations[0].Severity | Should -Be 'Information'
+        }
+    }
+
+    Context "When type constraints are used" {
+        It "Should flag disallowed type constraint" {
+            $def = 'function Test { param([System.Net.WebClient]$Client) }'
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $matchingViolations = $violations | Where-Object { $_.RuleName -eq $violationName }
+            $matchingViolations.Count | Should -BeGreaterThan 0
+            $matchingViolations[0].Message | Should -BeLike "*System.Net.WebClient*not permitted*"
+        }
+
+        It "Should NOT flag allowed type constraint" {
+            $def = 'function Test { param([string]$Name, [int]$Count, [hashtable]$Data) }'
+            $violations = Invoke-ScriptAnalyzer -ScriptDefinition $def -Settings $settings
+            $violations | Where-Object { $_.RuleName -eq $violationName } | Should -BeNullOrEmpty
         }
     }
 }
